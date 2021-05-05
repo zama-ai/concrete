60843145373a51d58ad7a44c8f0503871f65c27d
aPere3 <alexandre.pere@zama.ai>
2021-05-05 16:08:29 +0200

    bench(concrete-csprng): adds benchmarks to the prng crate

---
+++ concrete-csprng/benches/benchmark.rs
@@ -0,0 +1,34 @@
+use concrete_csprng::RandomGenerator;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+const N_GEN: usize = 1_000_000;
+
+fn unbounded_benchmark(c: &mut Criterion) {
+    let mut generator = RandomGenerator::new_hardware(None).unwrap();
+    c.bench_function("unbounded", |b| {
+        b.iter(|| {
+            (0..N_GEN).for_each(|_| {
+                generator.generate_next();
+            })
+        })
+    });
+}
+
+fn bounded_benchmark(c: &mut Criterion) {
+    let mut generator = RandomGenerator::new_hardware(None).unwrap();
+    let mut generator = generator
+        .try_fork(1, N_GEN * 10_000)
+        .unwrap()
+        .next()
+        .unwrap();
+    c.bench_function("unbounded", |b| {
+        b.iter(|| {
+            (0..N_GEN).for_each(|_| {
+                generator.generate_next();
+            })
+        })
+    });
+}
+
+criterion_group!(benches, unbounded_benchmark, bounded_benchmark);
+criterion_main!(benches);
