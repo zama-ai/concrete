d4b7d61f99dd25e4ea13504f262158f8c33aeea4
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2021-05-24 09:50:27 +0200

    chore(compiler): Add configuration of test and introduce command line options

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,31 +1,56 @@
 #include <iostream>
 
+#include <tclap/CmdLine.h>
+
 #include <mlir/Dialect/StandardOps/IR/Ops.h>
 #include <mlir/Parser.h>
 
 #include "zamalang/Dialect/HLFHE/IR/HLFHEDialect.h"
 #include "zamalang/Dialect/HLFHE/IR/HLFHETypes.h"
 
+struct CommandLineArgs {
+    std::vector<std::string> inputs;
+    std::string output;
+};
+
+void parseCommandLine(int argc, char** argv, CommandLineArgs* args)
+{
+    try {
+        TCLAP::CmdLine cmd("zamacompiler", ' ', "0.0.1");
+        // Input file names
+        TCLAP::UnlabeledMultiArg<std::string> fileNames("file", "The input files", false, "file");
+        cmd.add(fileNames);
+
+        // Output
+        TCLAP::ValueArg<std::string> output("o","out","Place the output into the <file>",false, "","string");
+        cmd.add(output);
+
+        cmd.parse( argc, argv );
+        args->output = output.getValue();
+        args->inputs = fileNames.getValue();
+
+    } catch (TCLAP::ArgException &e)  // catch exceptions
+    {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+        std::exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
-  mlir::MLIRContext context;
+    // Parse command line arguments
+    CommandLineArgs cmdLineArgs;
+    parseCommandLine(argc, argv, &cmdLineArgs);
 
-  // Load our Dialect in this MLIR Context.
-  context.getOrLoadDialect<mlir::zamalang::HLFHE::HLFHEDialect>();
-  context.getOrLoadDialect<mlir::StandardOpsDialect>();
+    // Initialize the MLIR context
+    mlir::MLIRContext context;
+    // Load our Dialect in this MLIR Context.
+    context.getOrLoadDialect<mlir::zamalang::HLFHE::HLFHEDialect>();
+    context.getOrLoadDialect<mlir::StandardOpsDialect>();
 
-  if(argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
-    return 1;
-  }
-
-  auto module = mlir::parseSourceFile<mlir::ModuleOp>(argv[1], &context);
-
-  if (!module) {
-    std::cerr << "Could not parse module" << std::endl;
-    return 1;
-  }
-
-  module->dump();
-
-  return 0;
+    // For all input file, parse and dump
+    for (const auto& fileName: cmdLineArgs.inputs) {
+        auto module = mlir::parseSourceFile<mlir::ModuleOp>(fileName, &context);
+        module->dump();
+    }
+    return 0;
 }
---
+++ compiler/tests/lit.cfg.py
@@ -0,0 +1,15 @@
+import lit.formats
+
+# Lit configuration
+config.name = "zamalang"
+config.test_format = lit.formats.ShTest("0")
+config.suffixes = {".mlir"}
+config.target_triple = ""
+
+# Set the llvm
+config.environment['PATH'] = os.pathsep.join([
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "build", "bin"),
+    os.path.join(os.environ['LLVM_PROJECT'], "build", "bin"),
+    config.environment['PATH']]
+)
+print(config.environment['PATH'])
