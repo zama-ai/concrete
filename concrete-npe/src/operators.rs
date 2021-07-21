ff01695a2dcdd810652133a113c79934b51e6e4e
J-B Orfila <jb.orfila@zama.ai>
2021-07-21 20:20:51 +0200

    get_variance -> get_modular_variance

--- concrete-npe/src/operators.rs
+++ concrete-npe/src/operators.rs
@@ -22,8 +22,8 @@ use concrete_commons::parameters::{
 /// let var2 = Variance::from_variance(2_f64.powf(-25.));
 /// let var_out = variance_add::<u64, _, _>(var1, var2);
 /// println!("Expect Variance (2^24) =  {}", f64::powi(2., -24));
-/// println!("Output Variance {}", var_out.get_variance());
-/// assert!((f64::powi(2., -24) - var_out.get_variance()).abs() < 0.0001);
+/// println!("Output Variance {}", var_out.get_modular_variance::<u64>());
+/// assert!((f64::powi(2., -24) - var_out.get_modular_variance::<u64>()).abs() < 0.0001);
 /// ```
 pub fn variance_add<T, D1, D2>(dispersion_ct1: D1, dispersion_ct2: D2) -> Variance
 where
@@ -71,11 +71,11 @@ where
 /// # Example:
 /// ```rust
 /// use concrete_commons::dispersion::*;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::*;
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let dimension = GlweDimension(3);
 /// let polynomial_size = PolynomialSize(1024);
 /// let base_log = DecompositionBaseLog(7);
@@ -137,10 +137,10 @@ where
                 * (1.
                     + dimension.0 as f64
                         * polynomial_size.0 as f64
-                        * (K::variance_key_coefficient::<T>().get_variance()
+                        * (K::variance_key_coefficient::<T>().get_modular_variance::<T>()
                             + square(K::expectation_key_coefficient())))
             + dimension.0 as f64 * polynomial_size.0 as f64 / 4.
-                * K::variance_key_coefficient::<T>().get_variance());
+                * K::variance_key_coefficient::<T>().get_modular_variance::<T>());
 
     Variance::from_modular_variance::<T>(res_1 + res_2)
 }
@@ -151,11 +151,11 @@ where
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::{DispersionParameter, Variance};
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::*;
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let dimension = GlweDimension(3);
 /// let l_gadget = DecompositionLevelCount(4);
 /// let base_log = DecompositionBaseLog(7);
@@ -164,7 +164,7 @@ where
 /// let dispersion_rlwe_0 = Variance::from_modular_variance::<u64>(f64::powi(2., 25));
 /// let dispersion_rlwe_1 = Variance::from_modular_variance::<u64>(f64::powi(2., 25));
 /// // Computing the noise
-/// let var_cmux = variance_cmux::<u64, _, _, _,BinaryKeyKind>(
+/// let var_cmux = variance_cmux::<u64, _, _, _, BinaryKeyKind>(
 ///     dimension,
 ///     polynomial_size,
 ///     base_log,
@@ -174,25 +174,24 @@ where
 ///     dispersion_rgsw,
 /// );
 /// let expected_variance = 6.04856441848937e23 + dispersion_rlwe_0.get_modular_variance::<u64>();
-/// println!(
-///     "Out. {}",
-///     var_cmux.get_modular_variance::<u64>()
-/// );
+/// println!("Out. {}", var_cmux.get_modular_variance::<u64>());
 ///
 /// println!("Exp. {}", expected_variance);
 ///
-/// let var_external_product = variance_external_product::<u64, _, _, BinaryKeyKind> (
-///         dimension,
-///         polynomial_size,
-///         base_log,
-///         l_gadget,
-///        dispersion_rgsw,
-///         variance_add::<u64, _, _>(dispersion_rlwe_0, dispersion_rlwe_1),
+/// let var_external_product = variance_external_product::<u64, _, _, BinaryKeyKind>(
+///     dimension,
+///     polynomial_size,
+///     base_log,
+///     l_gadget,
+///     dispersion_rgsw,
+///     variance_add::<u64, _, _>(dispersion_rlwe_0, dispersion_rlwe_1),
 /// );
-///  println!("External_Product. {}", var_external_product.get_modular_variance::<u64>());
+/// println!(
+///     "External_Product. {}",
+///     var_external_product.get_modular_variance::<u64>()
+/// );
 /// assert!(
-///     (expected_variance - var_cmux.get_modular_variance::<u64>()).abs()
-///         < f64::powi(10., 10)
+///     (expected_variance - var_cmux.get_modular_variance::<u64>()).abs() < f64::powi(10., 10)
 /// );
 /// ```
 pub fn variance_cmux<T, D1, D2, D3, K>(
@@ -306,11 +305,11 @@ where
 /// /// # Example:
 /// ```rust
 /// use concrete_commons::dispersion::*;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::*;
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let dimension = GlweDimension(3);
 /// let polynomial_size = PolynomialSize(1024);
 /// //Variance::from_variance(f64::powi(2., -38));
@@ -355,9 +354,10 @@ where
     let q_square = f64::powi(2., (2 * T::BITS) as i32);
     // #1
     let res_1 = big_n / delta_square
-        * (dispersion_glwe1.get_variance() * square(delta_2) * square(max_msg_2)
-            + dispersion_glwe2.get_variance() * square(delta_1) * square(max_msg_1)
-            + dispersion_glwe1.get_variance() * dispersion_glwe2.get_variance());
+        * (dispersion_glwe1.get_modular_variance::<T>() * square(delta_2) * square(max_msg_2)
+            + dispersion_glwe2.get_modular_variance::<T>() * square(delta_1) * square(max_msg_1)
+            + dispersion_glwe1.get_modular_variance::<T>()
+                * dispersion_glwe2.get_modular_variance::<T>());
 
     // #2
     let res_2 = (
@@ -408,11 +408,11 @@ where
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::*;
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let dimension = GlweDimension(3);
 /// let l_gadget = DecompositionLevelCount(4);
 /// let base_log = DecompositionBaseLog(7);
@@ -481,11 +481,11 @@ where
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::*;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::*;
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let dimension = GlweDimension(3);
 /// let polynomial_size = PolynomialSize(1024);
 /// //Variance::from_variance(f64::powi(2., -38));
@@ -596,11 +596,11 @@ where
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, LweDimension,
 /// };
 /// use concrete_npe::{variance_keyswitch_lwe_to_glwe_constant_term, *};
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let lwe_mask_size = LweDimension(630);
 /// let l_ks = DecompositionLevelCount(4);
 /// let base_log = DecompositionBaseLog(7);
@@ -644,16 +644,19 @@ where
     let res_3 = n / 4. * K::variance_key_coefficient::<T>().get_variance();
 
     // res 4
-    let res_4 = n * (level.0 as f64) * dispersion_ksk.get_variance() * (square(base) + 2.) / 12.;
+    let res_4 =
+        n * (level.0 as f64) * dispersion_ksk.get_modular_variance::<T>() * (square(base) + 2.)
+            / 12.;
 
     // return
-    Variance::from_variance(res_1.get_variance() + res_2 + res_3 + res_4)
+    Variance::from_variance(res_1.get_modular_variance::<T>() + res_2 + res_3 + res_4)
 }
 
 /// Return the variance of the non constant GLWE after an LWE to GLWE keyswitch
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, LweDimension,
 /// };
@@ -663,43 +666,47 @@ where
 /// let base_log = DecompositionBaseLog(7);
 /// let dispersion_ks = Variance::from_variance(f64::powi(2., -40));
 /// // Computing the noise
-/// let var_ks = variance_keyswitch_lwe_to_glwe_non_constant_terms(
+/// let var_ks = variance_keyswitch_lwe_to_glwe_non_constant_terms::<u64, _>(
 ///     lwe_mask_size,
 ///     dispersion_ks,
 ///     base_log,
 ///     l_ks,
 /// );
 /// ```
-pub fn variance_keyswitch_lwe_to_glwe_non_constant_terms<D>(
+pub fn variance_keyswitch_lwe_to_glwe_non_constant_terms<T, D>(
     lwe_mask_size: LweDimension,
     dispersion_ksk: D,
     base_log: DecompositionBaseLog,
     level: DecompositionLevelCount,
 ) -> Variance
 where
+    T: UnsignedInteger,
     D: DispersionParameter,
 {
     let n = lwe_mask_size.0 as f64;
     let base = (1 << base_log.0) as f64;
 
-    let res = n * (level.0 as f64) * dispersion_ksk.get_variance() * (square(base) + 2.) / 12.;
+    let res =
+        n * (level.0 as f64) * dispersion_ksk.get_modular_variance::<T>() * (square(base) + 2.)
+            / 12.;
 
     Variance::from_variance(res)
 }
 //TODO: THIS HAS TO BE RENAMED
 /// Returns a variance of U when doing a modulus switching
+/// U is the distribution of error for this bits greater than q.
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
-/// use concrete_commons::parameters::PolynomialSize;
-/// use concrete_npe::{variance_rlwe_k_1_var_u_mod_switch, *};
-/// use std::fmt::Binary;
 /// use concrete_commons::key_kinds::BinaryKeyKind;
+/// use concrete_commons::parameters::PolynomialSize;
+/// use concrete_npe::variance_u_rlwe_k_1_mod_switch;
+/// use std::fmt::Binary;
 /// let rlwe_mask_size = PolynomialSize(1024);
 /// ///
-/// let var_out = variance_rlwe_k_1_var_u_mod_switch::<u64, BinaryKeyKind>(rlwe_mask_size);
+/// let var_out = variance_u_rlwe_k_1_mod_switch::<u64, BinaryKeyKind>(rlwe_mask_size);
 /// ```
-pub fn variance_rlwe_k_1_var_u_mod_switch<T, K>(poly_size: PolynomialSize) -> Variance
+pub fn variance_u_rlwe_k_1_mod_switch<T, K>(poly_size: PolynomialSize) -> Variance
 where
     T: UnsignedInteger,
     K: KeyDispersion,
@@ -710,22 +717,23 @@ where
         1. / q_square
             * ((q_square - 1.) / 12.
                 * (1.
-                    + (poly_size.0 as f64) * K::variance_key_coefficient::<T>().get_variance()
+                    + (poly_size.0 as f64)
+                        * K::variance_key_coefficient::<T>().get_modular_variance::<T>()
                     + (poly_size.0 as f64) * square(K::expectation_key_coefficient()))
-                + (poly_size.0 as f64) / 4. * K::variance_key_coefficient::<T>().get_variance()),
+                / 4.
+                * K::variance_key_coefficient::<T>().get_modular_variance::<T>()),
     )
 }
 
-/// Return the variance when computing a relinarization of an RLWE resulting from
-/// a tensor product.
+/// Return the variance when computing a relinarization of aget_variance a tensor product.
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::{variance_keyswitch_lwe_to_glwe_constant_term, *};
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let poly_size = PolynomialSize(1024);
 /// let mask_size = GlweDimension(2);
 /// let level = DecompositionLevelCount(4);
@@ -760,7 +768,7 @@ where
     let res_1: f64 = k
         * (level.0 as f64)
         * big_n
-        * dispersion_rlk.get_variance()
+        * dispersion_rlk.get_modular_variance::<T>()
         * (square(basis) + 2.)
         * (k + 1.)
         / 24.;
@@ -819,11 +827,11 @@ pub fn cmux(
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::{variance_keyswitch_lwe_to_glwe_constant_term, *};
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let poly_size = PolynomialSize(1024);
 /// let mask_size = GlweDimension(2);
 /// let level = DecompositionLevelCount(4);
@@ -876,11 +884,11 @@ where
 /// # Example
 /// ```rust
 /// use concrete_commons::dispersion::Variance;
+/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// use concrete_commons::parameters::{
 ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, LweDimension, PolynomialSize,
 /// };
 /// use concrete_npe::{variance_keyswitch_lwe_to_glwe_constant_term, *};
-/// use concrete_commons::key_kinds::BinaryKeyKind;
 /// let poly_size = PolynomialSize(1024);
 /// let mask_size = LweDimension(2);
 /// let rlwe_mask_size = GlweDimension(2);
@@ -923,168 +931,3 @@ where
             .get_modular_variance::<T>(),
     )
 }
-
-/*
-/// Return the variance of output of a bootstrap given a set of parameters.
-/// To see how to use it, please refer to the test of the bootstrap.
-/// Arguments
-/// * `lwe_dimension` - size of the LWE mask
-/// * `rlwe_dimension` - size of the RLWE mask
-/// * `polynomial_size` - number of coefficients of the polynomial e.g. degree +
-///   1
-/// * `base_log` - decomposition base of the gadget matrix
-/// * `l_gadget` - number of elements for the decomposition
-/// * `dispersion_bsk` - dispersion of the bootstrapping key
-/// # Output
-/// * Returns the variance of the output RLWE
-/// # Example
-/// ```rust
-/// use concrete_commons::dispersion::{DispersionParameter, Variance};
-/// use concrete_commons::parameters::{
-///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, LweDimension, PolynomialSize,
-/// };
-/// use concrete_npe::bootstrap;
-/// let rlwe_dimension = GlweDimension(3);
-/// let lwe_dimension = LweDimension(630);
-/// let polynomial_size = PolynomialSize(1024);
-/// let base_log = DecompositionBaseLog(7);
-/// let l_gadget = DecompositionLevelCount(4);
-/// let var_bsk = Variance::from_variance(f64::powi(2., -38));
-/// // Computing the noise
-/// let var_bootstrap = bootstrap::<u64, _>(
-///     lwe_dimension,
-///     rlwe_dimension,
-///     polynomial_size,
-///     base_log,
-///     l_gadget,
-///     var_bsk,
-/// );
-/// ```
-//TODO: WHAT IS THIS FORMULA ?
-pub fn variance_bootstrap<T, D>(
-    lwe_dimension: LweDimension,
-    rlwe_dimension: GlweDimension,
-    polynomial_size: PolynomialSize,
-    base_log: DecompositionBaseLog,
-    l_gadget: DecompositionLevelCount,
-    var_bsk: D,
-) -> Variance
-where
-    T: UnsignedInteger,
-    D: DispersionParameter,
-{
-    let b_g = 1 << base_log.0;
-    let q_square = f64::powi(2., (2 * T::BITS) as i32);
-    let var_res_1: f64 = (lwe_dimension.0
-        * (rlwe_dimension.0 + 1)
-        * l_gadget.0
-        * polynomial_size.0
-        * (b_g * b_g + 2)) as f64
-        / 12.
-        * var_bsk.get_modular_variance::<T>();
-
-    let var_res_2: f64 = lwe_dimension.0 as f64
-        * ((rlwe_dimension.0 * polynomial_size.0 + 2) as f64
-            / (24. * f64::powi(b_g as f64, 2 * l_gadget.0 as i32)) as f64
-            + lwe_dimension.0 as f64 * (rlwe_dimension.0 * polynomial_size.0 / 48 - 1 / 12) as f64
-                / q_square);
-
-    Variance::from_variance(var_res_1 + var_res_2)
-}
-*/
-
-/*
-/// Return the variance of the keyswitch on a LWE ciphertext given a set of
-/// parameters. To see how to use it, please refer to the test of the
-/// keyswitch # Warning
-/// * This function compute the noise of the keyswitch without functional
-///   evaluation
-/// # Arguments
-/// `dimension_before` - size of the input LWE mask
-/// `l_ks` - number of level max for the torus decomposition
-/// `base_log` - number of bits for the base B (B=2^base_log)
-/// `dispersion_ks` - dispersion of the keyswitching key
-/// `dispersion_input` - dispersion of the input LWE
-/// # Example
-/// ```rust
-/// use concrete_commons::dispersion::Variance;
-/// use concrete_commons::parameters::{
-///     DecompositionBaseLog, DecompositionLevelCount, LweDimension,
-/// };
-/// use concrete_npe::key_switch;
-/// let dimension_before = LweDimension(630);
-/// let l_ks = DecompositionLevelCount(4);
-/// let base_log = DecompositionBaseLog(7);
-/// let dispersion_ks = Variance::from_variance(f64::powi(2., -38));
-/// let dispersion_input = Variance::from_variance(f64::powi(2., -40));
-/// // Computing the noise
-/// let var_ks = key_switch::<u64, _, _>(
-///     dimension_before,
-///     l_ks,
-///     base_log,
-///     dispersion_ks,
-///     dispersion_input,
-/// );
-/// ```
-pub fn variance_key_switch_lwe_to_lwe<T, D1, D2>(
-    dimension_before: LweDimension,
-    l_ks: DecompositionLevelCount,
-    base_log: DecompositionBaseLog,
-    dispersion_ks: D1,
-    dispersion_input: D2,
-) -> Variance
-    where
-        T: UnsignedInteger,
-        D1: DispersionParameter,
-        D2: DispersionParameter,
-{
-    let q_square = f64::powi(2., (2 * T::BITS * 8) as i32);
-    let var_res_1: f64 = dimension_before.0 as f64
-        * (1. / 24. * f64::powi(2.0, -2 * (base_log.0 * l_ks.0) as i32) + 1. / (48. * q_square));
-    let var_res_2: f64 = dimension_before.0 as f64
-        * l_ks.0 as f64
-        * (f64::powi(2., 2 * base_log.0 as i32) / 12. + 1. / 6.)
-        * dispersion_ks.get_modular_variance::<T>();
-
-    let var_res: f64 = dispersion_input.get_modular_variance::<T>() + var_res_1 + var_res_2;
-    Variance::from_variance(var_res)
-}
-*/
-
-/*
-/// returns a variance when computing a relinarization of an RLWE resulting from
-/// a tensor product: with k=2, and the secret key is (-S^2(X),S(X))
-/// input -> var_1 = 2^22 <=> std_dev lwe estimator 2^-53
-pub fn variance_rlwe_k_2_relinearization<T, D>(
-    poly_size: PolynomialSize,
-    dispersion_rlk: D,
-    base_log: DecompositionBaseLog,
-    level: DecompositionLevelCount,
-    key_type: KeyType,
-) -> Variance
-    where
-        T: UnsignedInteger,
-        D: DispersionParameter,
-{
-    let basis: f64 = (1 << base_log.0) as f64;
-    let big_n: f64 = poly_size.0 as f64;
-    let q_square = f64::powi(2., (2 * T::BITS) as i32);
-
-    // res 1
-    let res_1: f64 =
-        (level.0 as f64) * big_n * dispersion_rlk.get_variance() * (square(basis) + 2.) / 12.;
-
-    // res 2
-    let res_2: f64 = big_n / 2.
-        * (variance_odd_coefficient_in_polynomial_key_squared(poly_size, key_type)
-        + variance_even_coefficient_in_polynomial_key_squared(poly_size, key_type)
-        + 2. * square_expectation_mean_in_polynomial_key_times_key(poly_size, key_type))
-        * (q_square / (12. * f64::powi(basis, 2 * level.0 as i32)) - 1. / 12.)
-        + big_n / 8.
-        * (variance_odd_coefficient_in_polynomial_key_squared(poly_size, key_type)
-        + variance_even_coefficient_in_polynomial_key_squared(poly_size, key_type));
-
-    // return
-    Variance::from_variance(res_1 + res_2)
-}
-*/
