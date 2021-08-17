042f54afbf7e2879598a7d7c9b04b6cffd25a115
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2021-08-17 16:26:30 +0200

    fix(compiler/python): Update python test

--- compiler/tests/python/test_round_trip.py
+++ compiler/tests/python/test_round_trip.py
@@ -11,8 +11,8 @@ VALID_INPUTS = [
     }
     """,
     """
-    func @apply_lookup_table(%arg0: !HLFHE.eint<2>, %arg1: memref<4xi2>) -> !HLFHE.eint<2> {
-        %1 = "HLFHE.apply_lookup_table"(%arg0, %arg1): (!HLFHE.eint<2>, memref<4xi2>) -> (!HLFHE.eint<2>)
+    func @apply_lookup_table(%arg0: !HLFHE.eint<2>, %arg1: tensor<4xi2>) -> !HLFHE.eint<2> {
+        %1 = "HLFHE.apply_lookup_table"(%arg0, %arg1): (!HLFHE.eint<2>, tensor<4xi2>) -> (!HLFHE.eint<2>)
         return %1: !HLFHE.eint<2>
     }
     """,
