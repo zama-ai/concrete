312a30a4b5e6ef51a0e642323776248097047a8d
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2021-09-27 18:20:14 +0200

    test: add 124 - x in benchmark, compiles
    refs #471

---
+++ benchmarks/124_minus_x.py
@@ -0,0 +1,43 @@
+# Target: 124 - x
+
+import random
+
+import concrete.numpy as hnp
+
+
+def main():
+    def function_to_compile(x):
+        return 124 - x
+
+    x = hnp.EncryptedScalar(hnp.UnsignedInteger(3))
+
+    # Measure: Compilation Time (ms)
+    engine = hnp.compile_numpy_function(
+        function_to_compile,
+        {"x": x},
+        [(i,) for i in range(2 ** 3)],
+    )
+    # Measure: End
+
+    inputs = []
+    labels = []
+    for _ in range(4):
+        sample_x = random.randint(0, 2 ** 3 - 1)
+
+        inputs.append([sample_x])
+        labels.append(function_to_compile(*inputs[-1]))
+
+    correct = 0
+    for input_i, label_i in zip(inputs, labels):
+        # Measure: Evaluation Time (ms)
+        result_i = engine.run(*input_i)
+        # Measure: End
+
+        if result_i == label_i:
+            correct += 1
+
+    # Measure: Accuracy (%) = (correct / len(inputs)) * 100
+
+
+if __name__ == "__main__":
+    main()
