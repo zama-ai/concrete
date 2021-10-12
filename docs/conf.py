636da7808a3d21e6175c347b97b85c7acf572ee2
Arthur Meyre <arthur.meyre@zama.ai>
2021-10-12 15:42:48 +0200

    chore: update version to 0.2.0-rc2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Zama"
 author = "Zama"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0-rc1"
+release = "0.2.0-rc2"
 
 
 # -- General configuration ---------------------------------------------------
