e0e845bb8b56585d32afb1fcaa0b888f96af561b
Arthur Meyre <arthur.meyre@zama.ai>
2021-10-29 16:16:52 +0200

    chore: bump version to 0.2.0-rc5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Zama"
 author = "Zama"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0-rc4"
+release = "0.2.0-rc5"
 
 
 # -- General configuration ---------------------------------------------------
