8e8e777c3d38e48c8178776f5dc38508192b4325
Umut <umutsahin@protonmail.com>
2021-11-04 16:50:04 +0300

    chore(scripts): replace : with colon in measurement script

--- script/progress_tracker_utils/measure.py
+++ script/progress_tracker_utils/measure.py
@@ -20,6 +20,7 @@ def name_to_id(name):
     name = name.replace("/", "over")
     name = name.replace("%", "percent")
     name = name.replace("&", "and")
+    name = name.replace(":", "colon")
     name = name.replace(" ", "-")
     name = name.replace("(", "")
     name = name.replace(")", "")
