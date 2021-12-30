0f1dbd919dd490d22d9380c1d93cc495d6ed3125
rudy <rudy.sicard@zama.ai>
2021-12-30 20:31:49 +0100

    fix(format_cpp): More robust error reporting

--- .github/workflows/scripts/format_cpp.sh
+++ .github/workflows/scripts/format_cpp.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-set -o pipefail
+set -e -o pipefail
 
 find ./compiler/{include,lib,src} -iregex '^.*\.\(cpp\|cc\|h\|hpp\)$' | xargs clang-format -i -style='file'
-if [ $? -ne 0 ]
-then
-    exit 1
-fi
 
 # show changes if any
 git --no-pager diff
