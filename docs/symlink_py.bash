810f1ee9a3c404b848e61a38cd8fa45fccb3ac84
youben11 <ayoub.benaissa@zama.ai>
2022-01-11 14:46:57 +0100

    fix: python api docs was using old package naming

--- docs/symlink_py.bash
+++ docs/symlink_py.bash
@@ -6,14 +6,14 @@ cd links_to_compiler_build/py/concretelang_core
 
 ln -s ../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/mlir -f
 
-mkdir -p concretelang
+mkdir -p concrete/lang
 
-cd concretelang
+cd concrete
 
-ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_FHE_ops_gen.py fhe.py  -f
-ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_FHELinalg_ops_gen.py fhelinalg.py  -f
-ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_ods_common.py _ods_common.py  -f
+ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_FHE_ops_gen.py lang/fhe.py  -f
+ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_FHELinalg_ops_gen.py lang/fhelinalg.py  -f
+ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/lang/dialects/_ods_common.py lang/_ods_common.py  -f
 
 ln -s ../../../../../compiler/build/tools/concretelang/python_packages/concretelang_core/concrete/compiler.py compiler.py  -f
 
-touch __init__.py
+touch lang/__init__.py __init__.py
