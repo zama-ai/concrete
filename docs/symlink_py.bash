c33daaf0c894fa496ddc241bb7dffe16fac4d70b
Mayeul@Zama <mayeul.debellabre@zama.ai>
2022-01-12 10:44:51 +0100

    fix(doc): exit if broken link detected

--- docs/symlink_py.bash
+++ docs/symlink_py.bash
@@ -1,5 +1,14 @@
 #!/bin/bash 
 
+create_check_symlink () {
+   ln -s $1 $2 -f
+   if ! [ -e ${2} ] ; then
+      echo "Broken link"
+      echo $2
+      exit 1
+   fi
+}
+
 mkdir -p links_to_compiler_build/py/concretelang_core
 
 cd links_to_compiler_build/py/concretelang_core
@@ -17,10 +26,11 @@ pyfiles=`find $py_prefix -iname "*.py"`
 
 for file in $pyfiles
 do
-    ln -s $file `echo $file | sed s:$py_prefix::` -f
+    relative_path=`echo $file | sed s:$py_prefix::`
+    create_check_symlink $file $relative_path
 done
 
 # Manually create dialect files
-ln -s ${py_prefix}lang/dialects/_FHE_ops_gen.py lang/dialects/fhe.py  -f
-ln -s ${py_prefix}lang/dialects/_FHELinalg_ops_gen.py lang/dialects/fhelinalg.py  -f
-ln -s ${py_prefix}lang/dialects/_ods_common.py lang/dialects/_ods_common.py  -f
+create_check_symlink ${py_prefix}lang/dialects/_FHE_ops_gen.py lang/dialects/fhe.py
+create_check_symlink ${py_prefix}lang/dialects/_FHELinalg_ops_gen.py lang/dialects/fhelinalg.py
+create_check_symlink ${py_prefix}lang/dialects/_ods_common.py lang/dialects/_ods_common.py
