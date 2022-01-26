96c10156a55be5ca221783f36664143271644ede
aquint-zama <alexandre.quint@zama.ai>
2022-01-26 13:31:01 +0100

    docs(boolean): add API and game of life link

--- concrete-boolean/docs/conf.py
+++ concrete-boolean/docs/conf.py
@@ -58,7 +58,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_zama_theme'
 html_static_path = ['_static']
-html_logo = '_static/logo.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
--- concrete-boolean/docs/user/index.rst
+++ concrete-boolean/docs/user/index.rst
@@ -1,5 +1,5 @@
-Summary
-=======
+User Guide
+==========
 
 
 .. toctree::
@@ -8,5 +8,6 @@ Summary
    introduction.md
    how_does_it_work.md
    tutorial.md
+   Tutorial: Concrete Boolean and Conway’s Game of Life <https://medium.com/zama-ai/concrete-boolean-and-conways-game-of-life-a-tutorial-f2bcfd614131>
    error.md
    Advanced Topics <advanced_topics/index>
