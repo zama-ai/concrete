42ed61b665b050df8ca5f033f2e31e1d1d46fa00
Agnes Leroy <agnes.leroy@zama.ai>
2022-01-27 13:32:04 +0100

    chore(concrete_core): expose concrete-commons types in concrete-core
    To simplify dependencies in the project we expose concrete-commons types
    in the prelude of concrete-core. This will avoid having to add
    concrete-commons as a dependency for crates built on top of
    concrete-core.

--- concrete-core/src/prelude.rs
+++ concrete-core/src/prelude.rs
@@ -1,9 +1,16 @@
 #![doc(hidden)]
 
-pub use super::specification::engines::*;
-pub use super::specification::entities::*;
+// Expose concrete_commons types in the prelude
+// This avoids having to add concrete-commons as a dependency
+// in crates built on top of concrete-core
+pub use concrete_commons::dispersion::*;
+pub use concrete_commons::key_kinds::*;
+pub use concrete_commons::parameters::*;
+pub use concrete_commons::*;
 
 #[cfg(feature = "backend_core")]
 pub use super::backends::core::engines::*;
 #[cfg(feature = "backend_core")]
 pub use super::backends::core::entities::*;
+pub use super::specification::engines::*;
+pub use super::specification::entities::*;
