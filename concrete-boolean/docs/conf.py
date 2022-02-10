e7e1eed1499026e070155eaccb1578cd8dd70861
aquint-zama <alexandre.quint@zama.ai>
2022-02-10 14:46:19 +0100

    docs(boolean): enable analytics

--- concrete-boolean/docs/conf.py
+++ concrete-boolean/docs/conf.py
@@ -71,7 +71,7 @@ html_theme_options = {
     }],
     "navigation_depth": 2,
     "collapse_navigation": False,
-    # "google_analytics_id": "G-XRM93J9QBW",
+    "google_analytics_id": "G-XRM93J9QBW",
 }
 html_context = {
     "show_version": True,
