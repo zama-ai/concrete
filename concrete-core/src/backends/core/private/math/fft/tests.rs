8c3821009ef8099bd577c356c3e7f44aa4b4f0f6
Alexandre Péré <alexandre.pere@zama.ai>
2022-02-24 08:36:25 +0100

    fix(concrete_core): remove obsolete SerializeComplex64
    Before switching to `concrete-fftw`, a structure `SerializeComplex64` was used in
    `concrete-core` to serialize aligned vecs of complexs. It is not useful
    anymore. This commit removes it.

--- concrete-core/src/backends/core/private/math/fft/tests.rs
+++ concrete-core/src/backends/core/private/math/fft/tests.rs
@@ -1,6 +1,4 @@
 use crate::backends::core::private::math::fft::twiddles::{BackwardCorrector, ForwardCorrector};
-#[cfg(feature = "serde_serialize")]
-use crate::backends::core::private::math::fft::SerializableComplex64;
 use crate::backends::core::private::math::fft::{Complex64, Fft, FourierPolynomial};
 use crate::backends::core::private::math::polynomial::Polynomial;
 use crate::backends::core::private::math::random::RandomGenerator;
@@ -8,8 +6,6 @@ use crate::backends::core::private::math::tensor::{AsMutTensor, AsRefTensor};
 use concrete_commons::numeric::Numeric;
 use concrete_commons::parameters::PolynomialSize;
 use concrete_fftw::array::AlignedVec;
-#[cfg(feature = "serde_serialize")]
-use serde_test::{assert_tokens, Token};
 
 #[test]
 fn test_single_forward_backward() {
@@ -132,23 +128,4 @@ fn test_two_forward_backward() {
                 .for_each(|(exp, out)| assert!((exp - out).abs() < 1e-12f64));
         }
     }
-}
-
-#[cfg(feature = "serde_serialize")]
-#[test]
-fn test_ser_de_complex64() {
-    let x = SerializableComplex64(Complex64 {
-        re: 1.234,
-        im: 5.678,
-    });
-
-    assert_tokens(
-        &x,
-        &[
-            Token::Tuple { len: 2 },
-            Token::F64(1.234),
-            Token::F64(5.678),
-            Token::TupleEnd,
-        ],
-    );
 }
