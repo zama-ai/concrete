635c3434e8c5582dc9596b20ff78b64a0ea9aa38
Michael Walter <michael.walter@zama.ai>
2022-03-09 15:39:01 +0100

    test(core): adds a fixture for the LweCiphertextPlaintextDiscardingSubtractionEngine and adds a test using it to concrete-core-test

--- concrete-core-fixture/src/fixture/mod.rs
+++ concrete-core-fixture/src/fixture/mod.rs
@@ -298,6 +298,9 @@ pub use lwe_ciphertext_plaintext_discarding_addition::*;
 mod lwe_ciphertext_plaintext_fusing_addition;
 pub use lwe_ciphertext_plaintext_fusing_addition::*;
 
+mod lwe_ciphertext_plaintext_discarding_subtraction;
+pub use lwe_ciphertext_plaintext_discarding_subtraction::*;
+
 mod lwe_ciphertext_vector_encryption;
 pub use lwe_ciphertext_vector_encryption::*;
 
