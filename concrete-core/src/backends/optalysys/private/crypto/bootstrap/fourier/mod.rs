ebeda6dc6d5c19548330f4a18f8787f5cae04e1e
Florent Michel <florent.michel@optalysys.com>
2022-03-22 17:38:34 +0000

    update doc tests in fourier/mod.rs

--- concrete-core/src/backends/optalysys/private/crypto/bootstrap/fourier/mod.rs
+++ concrete-core/src/backends/optalysys/private/crypto/bootstrap/fourier/mod.rs
@@ -60,7 +60,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -111,7 +111,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let vector = vec![Complex64::new(0., 0.); 256 * 5 * 4 * 4 * 15];
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::from_container(
@@ -155,36 +155,6 @@ where
 
     /// Fills a fourier bootstrapping key with the fourier transform of a bootstrapping key in
     /// coefficient domain.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use concrete_commons::parameters::{
-    ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
-    /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::{
-    ///     FourierBootstrapKey, FourierBuffers, StandardBootstrapKey,
-    /// };
-    /// use concrete_core::backends::core::private::math::fft::Complex64;
-    /// let bsk = StandardBootstrapKey::allocate(
-    ///     9u32,
-    ///     GlweSize(7),
-    ///     PolynomialSize(256),
-    ///     DecompositionLevelCount(3),
-    ///     DecompositionBaseLog(5),
-    ///     LweDimension(4),
-    /// );
-    /// let mut frr_bsk = FourierBootstrapKey::allocate(
-    ///     Complex64::new(0., 0.),
-    ///     GlweSize(7),
-    ///     PolynomialSize(256),
-    ///     DecompositionLevelCount(3),
-    ///     DecompositionBaseLog(5),
-    ///     LweDimension(4),
-    /// );
-    /// let mut buffers = FourierBuffers::new(frr_bsk.polynomial_size(), frr_bsk.glwe_size());
-    /// frr_bsk.fill_with_forward_fourier(&bsk, &mut buffers);
-    /// ```
     pub fn fill_with_forward_fourier<InputCont>(
         &mut self,
         coef_bsk: &StandardBootstrapKey<InputCont>,
@@ -220,7 +190,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -244,7 +214,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -268,7 +238,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -292,7 +262,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -316,7 +286,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -340,7 +310,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -375,7 +345,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// let bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
     ///     Complex64::new(9., 8.),
@@ -421,7 +391,7 @@ where
     /// use concrete_commons::parameters::{
     ///     DecompositionBaseLog, DecompositionLevelCount, GlweSize, LweDimension, PolynomialSize,
     /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::FourierBootstrapKey;
+    /// use concrete_core::backends::optalysys::private::crypto::bootstrap::FourierBootstrapKey;
     /// use concrete_core::backends::core::private::math::fft::Complex64;
     /// use concrete_core::backends::core::private::math::tensor::{AsMutTensor, AsRefTensor};
     /// let mut bsk: FourierBootstrapKey<_, u32> = FourierBootstrapKey::allocate(
@@ -763,92 +733,6 @@ where
     Scalar: UnsignedTorus,
 {
     /// Performs a bootstrap of an lwe ciphertext, with a given accumulator.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// use concrete_commons::dispersion::LogStandardDev;
-    /// use concrete_commons::numeric::CastInto;
-    /// use concrete_commons::parameters::{
-    ///     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, LweDimension, LweSize,
-    ///     PolynomialSize,
-    /// };
-    /// use concrete_core::backends::core::private::crypto::bootstrap::{
-    ///     FourierBootstrapKey, FourierBuffers, StandardBootstrapKey,
-    /// };
-    /// use concrete_core::backends::core::private::crypto::encoding::Plaintext;
-    /// use concrete_core::backends::core::private::crypto::glwe::GlweCiphertext;
-    /// use concrete_core::backends::core::private::crypto::lwe::LweCiphertext;
-    /// use concrete_core::backends::core::private::crypto::secret::generators::{
-    ///     EncryptionRandomGenerator, SecretRandomGenerator,
-    /// };
-    /// use concrete_core::backends::core::private::crypto::secret::{GlweSecretKey, LweSecretKey};
-    /// use concrete_core::backends::core::private::math::fft::Complex64;
-    /// use concrete_core::backends::core::private::math::tensor::AsMutTensor;
-    ///
-    /// // define settings
-    /// let polynomial_size = PolynomialSize(1024);
-    /// let rlwe_dimension = GlweDimension(1);
-    /// let lwe_dimension = LweDimension(630);
-    ///
-    /// let level = DecompositionLevelCount(3);
-    /// let base_log = DecompositionBaseLog(7);
-    /// let std = LogStandardDev::from_log_standard_dev(-29.);
-    ///
-    /// let mut secret_generator = SecretRandomGenerator::new(None);
-    /// let mut encryption_generator = EncryptionRandomGenerator::new(None);
-    ///
-    /// let mut rlwe_sk =
-    ///     GlweSecretKey::generate_binary(rlwe_dimension, polynomial_size, &mut secret_generator);
-    /// let mut lwe_sk = LweSecretKey::generate_binary(lwe_dimension, &mut secret_generator);
-    ///
-    /// // allocation and generation of the key in coef domain:
-    /// let mut coef_bsk = StandardBootstrapKey::allocate(
-    ///     0 as u32,
-    ///     rlwe_dimension.to_glwe_size(),
-    ///     polynomial_size,
-    ///     level,
-    ///     base_log,
-    ///     lwe_dimension,
-    /// );
-    /// coef_bsk.fill_with_new_key(&lwe_sk, &rlwe_sk, std, &mut encryption_generator);
-    ///
-    /// // allocation for the bootstrapping key
-    /// let mut fourier_bsk = FourierBootstrapKey::allocate(
-    ///     Complex64::new(0., 0.),
-    ///     rlwe_dimension.to_glwe_size(),
-    ///     polynomial_size,
-    ///     level,
-    ///     base_log,
-    ///     lwe_dimension,
-    /// );
-    ///
-    /// let mut buffers =
-    ///     FourierBuffers::new(fourier_bsk.polynomial_size(), fourier_bsk.glwe_size());
-    /// fourier_bsk.fill_with_forward_fourier(&coef_bsk, &mut buffers);
-    ///
-    /// let message = Plaintext(2u32.pow(30));
-    ///
-    /// let mut lwe_in = LweCiphertext::allocate(0u32, lwe_dimension.to_lwe_size());
-    /// let mut lwe_out =
-    ///     LweCiphertext::allocate(0u32, LweSize(rlwe_dimension.0 * polynomial_size.0 + 1));
-    /// lwe_sk.encrypt_lwe(&mut lwe_in, &message, std, &mut encryption_generator);
-    ///
-    /// // accumulator is a trivial encryption of [0, 1/2N, 2/2N, ...]
-    /// let mut accumulator =
-    ///     GlweCiphertext::allocate(0u32, polynomial_size, rlwe_dimension.to_glwe_size());
-    /// accumulator
-    ///     .get_mut_body()
-    ///     .as_mut_tensor()
-    ///     .iter_mut()
-    ///     .enumerate()
-    ///     .for_each(|(i, a)| {
-    ///         *a = (i as f64 * 2_f64.powi(32_i32 - 10 - 1)).cast_into();
-    ///     });
-    ///
-    /// // bootstrap
-    /// fourier_bsk.bootstrap(&mut lwe_out, &lwe_in, &accumulator, &mut buffers);
-    /// ```
     pub fn bootstrap<C1, C2, C3>(
         &self,
         lwe_out: &mut LweCiphertext<C1>,
