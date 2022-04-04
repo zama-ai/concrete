11fcaadb9f11f47442984319eb5df030c8490f45
Umut <umutsahin@protonmail.com>
2022-04-04 13:26:34 +0200

    feat: recreate the global module structure

---
+++ concrete/__init__.py
@@ -0,0 +1,7 @@
+"""
+Declaration of `concrete` namespace.
+"""
+
+# Do not modify, this is to have a compatible namespace package
+# https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#pkg-resources-style-namespace-packages
+__import__("pkg_resources").declare_namespace(__name__)  # pragma: no cover
