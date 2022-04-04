f5431be60225ff0c8ebe72d2daea88f7ec13c726
Umut <umutsahin@protonmail.com>
2022-04-04 13:30:51 +0200

    feat: export appropriate items from concrete.numpy module

--- concrete/numpy/__init__.py
+++ concrete/numpy/__init__.py
@@ -1,3 +1,15 @@
 """
 Declaration of `concrete.numpy` namespace.
 """
+
+from .compilation import (
+    Circuit,
+    CompilationArtifacts,
+    CompilationConfiguration,
+    Compiler,
+    EncryptionStatus,
+    compiler,
+)
+from .extensions import LookupTable, conv2d
+from .mlir.utils import MAXIMUM_BIT_WIDTH
+from .representation import Graph
