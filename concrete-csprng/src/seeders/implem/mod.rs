zama-ai/concrete-core

feat(concrete_csprng): refactor the csprng to prepare core backend split
BREAKING_CHANGE: this commit completely breaks the previous API

concrete-csprng/src/seeders/implem/mod.rs
@@ -1,0 +1,9 @@
+#[cfg(feature = "seeder_x86_64_rdseed")]
+mod rdseed;
+#[cfg(feature = "seeder_x86_64_rdseed")]
+pub use rdseed::RdseedSeeder;
+
+#[cfg(feature = "seeder_linux")]
+mod linux;
+#[cfg(feature = "seeder_linux")]
+pub use linux::LinuxSeeder;

zama-ai/concrete

concrete-csprng/src/seeders/implem/mod.rs
@@ -1,0 +1,9 @@
+#[cfg(feature = "seeder_x86_64_rdseed")]
+mod rdseed;
+#[cfg(feature = "seeder_x86_64_rdseed")]
+pub use rdseed::RdseedSeeder;
+
+#[cfg(feature = "seeder_unix")]
+mod unix;
+#[cfg(feature = "seeder_unix")]
+pub use unix::UnixSeeder;