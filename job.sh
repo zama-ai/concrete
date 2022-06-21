40c0cc6744e995e244622cee926f7118fbcd85e7
Ben <ben.curtis@zama.ai>
2022-06-21 15:03:18 +0100

    correct bounds

--- job.sh
+++ job.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 # 80-bits
 sage-python new_scripts.py 80 2 12
-sage-python new_scripts.py 80 13 22
-sage-python new_scripts.py 80 23 32
-sage-python new_scripts.py 80 33 42
-sage-python new_scripts.py 80 43 52
-sage-python new_scripts.py 80 53 59
+sage-python new_scripts.py 80 12 22
+sage-python new_scripts.py 80 22 32
+sage-python new_scripts.py 80 32 42
+sage-python new_scripts.py 80 42 52
+sage-python new_scripts.py 80 52 59
 # 128-bits
 sage-python new_scripts.py 128 2 12
-sage-python new_scripts.py 128 13 22
-sage-python new_scripts.py 128 23 32
-sage-python new_scripts.py 128 33 42
-sage-python new_scripts.py 128 43 52
-sage-python new_scripts.py 128 53 59
+sage-python new_scripts.py 128 12 22
+sage-python new_scripts.py 128 22 32
+sage-python new_scripts.py 128 32 42
+sage-python new_scripts.py 128 42 52
+sage-python new_scripts.py 128 52 59
 # 192-bits
 sage-python new_scripts.py 192 2 12
-sage-python new_scripts.py 192 13 22
-sage-python new_scripts.py 192 23 32
-sage-python new_scripts.py 192 33 42
-sage-python new_scripts.py 192 43 52
-sage-python new_scripts.py 192 53 59
+sage-python new_scripts.py 192 12 22
+sage-python new_scripts.py 192 22 32
+sage-python new_scripts.py 192 32 42
+sage-python new_scripts.py 192 42 52
+sage-python new_scripts.py 192 52 59
 # 256-bits
 sage-python new_scripts.py 256 2 12
-sage-python new_scripts.py 256 13 22
-sage-python new_scripts.py 256 23 32
-sage-python new_scripts.py 256 33 42
-sage-python new_scripts.py 256 43 52
-sage-python new_scripts.py 256 53 59
+sage-python new_scripts.py 256 12 22
+sage-python new_scripts.py 256 22 32
+sage-python new_scripts.py 256 32 42
+sage-python new_scripts.py 256 42 52
+sage-python new_scripts.py 256 52 59
 
 
