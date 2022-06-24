004c576ad73b22a5bc922f4130a0946eb90b0fe7
Ben <ben.curtis@zama.ai>
2022-06-24 13:30:13 +0100

    update files

--- job.sh
+++ job.sh
@@ -1,66 +1,66 @@
 #!/bin/sh
 # 80-bits
-#sage new_scripts.py 80 2 12
-#sage new_scripts.py 80 12 22
-#sage new_scripts.py 80 22 32
-#sage new_scripts.py 80 32 42
-#sage new_scripts.py 80 42 52
-#sage new_scripts.py 80 52 59
+sage generate_data.py 80 2 12
+sage generate_data.py 80 12 22
+sage generate_data.py 80 22 32
+sage generate_data.py 80 32 42
+sage generate_data.py 80 42 52
+sage generate_data.py 80 52 59
 # 96-bits
-sage new_scripts.py 96 2 12
-sage new_scripts.py 96 12 22
-sage new_scripts.py 96 22 32
-sage new_scripts.py 96 32 42
-sage new_scripts.py 96 42 52
-sage new_scripts.py 96 52 59
+sage generate_data.py 96 2 12
+sage generate_data.py 96 12 22
+sage generate_data.py 96 22 32
+sage generate_data.py 96 32 42
+sage generate_data.py 96 42 52
+sage generate_data.py 96 52 59
 # 112-bits
-sage new_scripts.py 112 2 12
-sage new_scripts.py 112 12 22
-sage new_scripts.py 112 22 32
-sage new_scripts.py 112 32 42
-sage new_scripts.py 112 42 52
-sage new_scripts.py 112 52 59
+sage generate_data.py 112 2 12
+sage generate_data.py 112 12 22
+sage generate_data.py 112 22 32
+sage generate_data.py 112 32 42
+sage generate_data.py 112 42 52
+sage generate_data.py 112 52 59
 # 128-bits
-#sage new_scripts.py 128 2 12
-#sage new_scripts.py 128 12 22
-#sage new_scripts.py 128 22 32
-#sage new_scripts.py 128 32 42
-#sage new_scripts.py 128 42 52
-#sage new_scripts.py 128 52 59
+sage generate_data.py 128 2 12
+sage generate_data.py 128 12 22
+sage generate_data.py 128 22 32
+sage generate_data.py 128 32 42
+sage generate_data.py 128 42 52
+sage generate_data.py 128 52 59
 # 144-bits
-sage new_scripts.py 144 2 12
-sage new_scripts.py 144 12 22
-sage new_scripts.py 144 22 32
-sage new_scripts.py 144 32 42
-sage new_scripts.py 144 42 52
-sage new_scripts.py 144 52 59
+sage generate_data.py 144 2 12
+sage generate_data.py 144 12 22
+sage generate_data.py 144 22 32
+sage generate_data.py 144 32 42
+sage generate_data.py 144 42 52
+sage generate_data.py 144 52 59
 # 160-bits
-sage new_scripts.py 160 2 12
-sage new_scripts.py 160 12 22
-sage new_scripts.py 160 22 32
-sage new_scripts.py 160 32 42
-sage new_scripts.py 160 42 52
-sage new_scripts.py 160 52 59
+sage generate_data.py 160 2 12
+sage generate_data.py 160 12 22
+sage generate_data.py 160 22 32
+sage generate_data.py 160 32 42
+sage generate_data.py 160 42 52
+sage generate_data.py 160 52 59
 # 176-bits
-sage new_scripts.py 176 2 12
-sage new_scripts.py 176 12 22
-sage new_scripts.py 176 22 32
-sage new_scripts.py 176 32 42
-sage new_scripts.py 176 42 52
-sage new_scripts.py 176 52 59
+sage generate_data.py 176 2 12
+sage generate_data.py 176 12 22
+sage generate_data.py 176 22 32
+sage generate_data.py 176 32 42
+sage generate_data.py 176 42 52
+sage generate_data.py 176 52 59
 # 192-bits
-#sage new_scripts.py 192 2 12
-#sage new_scripts.py 192 12 22
-#sage new_scripts.py 192 22 32
-#sage new_scripts.py 192 32 42
-#sage new_scripts.py 192 42 52
-#sage new_scripts.py 192 52 59
+sage generate_data.py 192 2 12
+sage generate_data.py 192 12 22
+sage generate_data.py 192 22 32
+sage generate_data.py 192 32 42
+sage generate_data.py 192 42 52
+sage generate_data.py 192 52 59
 # 256-bits
-#sage new_scripts.py 256 2 12
-#sage new_scripts.py 256 12 22
-#sage new_scripts.py 256 22 32
-#sage new_scripts.py 256 32 42
-#sage new_scripts.py 256 42 52
-#sage new_scripts.py 256 52 59
+sage generate_data.py 256 2 12
+sage generate_data.py 256 12 22
+sage generate_data.py 256 22 32
+sage generate_data.py 256 32 42
+sage generate_data.py 256 42 52
+sage generate_data.py 256 52 59
 
 
