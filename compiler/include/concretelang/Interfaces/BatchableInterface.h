3ce7c96f3f8510652835363b5c8dc77b0cbf8e91
Andi Drebes <andi.drebes@zama.ai>
2022-11-10 16:39:06 +0100

    feat(compiler): Add operation interface for batchable operations
    This adds a new operation interface that allows an operation to
    specify that a batched version of the operation exists that applies it
    on the elements of a flat tensor in parallel.

---
+++ compiler/include/concretelang/Interfaces/BatchableInterface.h
@@ -0,0 +1,14 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_INTERFACES_BATCHABLEINTERFACE_H_
+#define CONCRETELANG_INTERFACES_BATCHABLEINTERFACE_H_
+
+#include <mlir/IR/ImplicitLocOpBuilder.h>
+#include <mlir/IR/OpDefinition.h>
+
+#include <concretelang/Interfaces/BatchableInterface.h.inc>
+
+#endif // CONCRETELANG_INTERFACES_BATCHABLEINTERFACE_H_
---
+++ compiler/lib/Interfaces/BatchableInterface.cpp
@@ -0,0 +1,8 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#include <concretelang/Interfaces/BatchableInterface.h>
+
+#include <concretelang/Interfaces/BatchableInterface.cpp.inc>
