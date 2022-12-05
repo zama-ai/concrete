351bab0bc9d6d9c1f0315e3563f830173336bdfd
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2022-12-05 16:22:09 +0100

    chore(tests): Add a make target for testing gpu

--- compiler/tests/end_to_end_fixture/end_to_end_apply_lookup_table_gen.py
+++ compiler/tests/end_to_end_fixture/end_to_end_apply_lookup_table_gen.py
@@ -1,43 +1,50 @@
 from platform import mac_ver
 import numpy as np
+import argparse
+
 
 MIN_PRECISON = 1
 MAX_PRECISION = 16
 
 
-def main():
+def generate(args):
     print("# /!\ DO NOT EDIT MANUALLY THIS FILE MANUALLY")
     print("# /!\ THIS FILE HAS BEEN GENERATED")
     np.random.seed(0)
-    for p in range(MIN_PRECISON, MAX_PRECISION+1):
-        if p != 1:
-            print("---")
+    for p in args.bitwidth:
         max_value = (2 ** p) - 1
         random_lut = np.random.randint(max_value+1, size=2**p)
-        print("description: apply_lookup_table_{0}bits".format(p))
+        print(f"description: apply_lookup_table_{p}bits")
         print("program: |")
         print(
-            "  func.func @main(%arg0: !FHE.eint<{0}>) -> !FHE.eint<{0}> {{".format(p))
-        print("    %tlu = arith.constant dense<[{0}]> : tensor<{1}xi64>".format(
-            ','.join(map(str, random_lut)), 2**p))
+            f"  func.func @main(%arg0: !FHE.eint<{p}>) -> !FHE.eint<{p}> {{")
+        print(f"    %tlu = arith.constant dense<[{','.join(map(str, random_lut))}]> : tensor<{2**p}xi64>")
         print(
-            "    %1 = \"FHE.apply_lookup_table\"(%arg0, %tlu): (!FHE.eint<{0}>, tensor<{1}xi64>) -> (!FHE.eint<{0}>)".format(p, 2**p))
-        print("    return %1: !FHE.eint<{0}>".format(p))
+            f"    %1 = \"FHE.apply_lookup_table\"(%arg0, %tlu): (!FHE.eint<{p}>, tensor<{2**p}xi64>) -> (!FHE.eint<{p}>)")
+        print(f"    return %1: !FHE.eint<{p}>")
         print("  }")
         print("tests:")
         print("  - inputs:")
         print("    - scalar: 0")
         print("    outputs:")
-        print("    - scalar: {0}".format(random_lut[0]))
+        print(f"    - scalar: {random_lut[0]}")
         print("  - inputs:")
         random_i = np.random.randint(max_value)
-        print("    - scalar: {0}".format(random_i))
+        print(f"    - scalar: {random_i}")
         print("    outputs:")
-        print("    - scalar: {0}".format(random_lut[random_i]))
+        print(f"    - scalar: {random_lut[random_i]}")
         print("  - inputs:")
-        print("    - scalar: {0}".format(max_value))
+        print(f"    - scalar: {max_value}")
         print("    outputs:")
-        print("    - scalar: {0}".format(random_lut[max_value]))
+        print(f"    - scalar: {random_lut[max_value]}")
+        print("---")
 
-
-main()
+CLI = argparse.ArgumentParser()
+CLI.add_argument(
+    "--bitwidth",
+    help="Specify the list of bitwidth to generate",
+    nargs="+",
+    type=int,
+    default=list(range(1,16)),
+)
+generate(CLI.parse_args())
