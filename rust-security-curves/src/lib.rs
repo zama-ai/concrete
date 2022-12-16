732f2de57d40ce349d83eda3a2af8cf4cb8afb1a
Mayeul@Zama <mayeul.debellabre@zama.ai>
2022-12-16 11:06:39 +0100

    rust: update rust security curves generation

--- rust-security-curves/src/lib.rs
+++ rust-security-curves/src/lib.rs
@@ -1,4 +1,4 @@
-const SECURITY_WEIGHTS_ARRAY: [(f64, f64, u64, &str, u64); 9] = include!("../verified_curves.txt");
+const SECURITY_WEIGHTS_ARRAY: [(u64, SecurityWeights); 9] = include!("../verified_curves.txt");
 
 #[derive(Clone, Copy)]
 pub struct SecurityWeights {
@@ -27,29 +27,15 @@ impl SecurityWeights {
 pub fn supported_security_levels() -> impl std::iter::Iterator<Item = u64> {
     SECURITY_WEIGHTS_ARRAY
         .iter()
-        .filter(|(_, _, _, status, _)| *status == "PASS")
-        .map(|(_, _, security_level, _, _)| *security_level)
+        .map(|(security_level, _)| *security_level)
 }
 
 pub fn security_weight(security_level: u64) -> Option<SecurityWeights> {
     let index = SECURITY_WEIGHTS_ARRAY
-        .binary_search_by_key(&security_level, |(_, _, security_level, _, _)| {
-            *security_level
-        })
+        .binary_search_by_key(&security_level, |(security_level, _)| *security_level)
         .ok()?;
 
-    let (slope, bias, _security_level, status, minimal_lwe_dimension) =
-        SECURITY_WEIGHTS_ARRAY[index];
-
-    if status == "PASS" {
-        Some(SecurityWeights {
-            slope,
-            bias,
-            minimal_lwe_dimension,
-        })
-    } else {
-        None
-    }
+    Some(SECURITY_WEIGHTS_ARRAY[index].1)
 }
 
 #[cfg(test)]
