e1cbed78673ad389ae6e1b445a93d6e76f74a5d1
Mayeul@Zama <mayeul.debellabre@zama.ai>
2022-12-20 17:20:35 +0100

    fix(rust): fix hardcoded array size

--- concrete-security-curves-rust/src/curves.gen.rs
+++ concrete-security-curves-rust/src/curves.gen.rs
@@ -1,6 +1,6 @@
-[
+const SECURITY_WEIGHTS_ARRAY: [(u64, SecurityWeights); 4] = [
     (80, SecurityWeights { slope: -0.0404263311936459, bias: 1.660978864143658, minimal_lwe_dimension: 450 }),
     (112, SecurityWeights { slope: -0.02967013708113588, bias: 2.16246371408387, minimal_lwe_dimension: 450 }),
     (128, SecurityWeights { slope: -0.026405028765226296, bias: 2.482642269104389, minimal_lwe_dimension: 450 }),
     (192, SecurityWeights { slope: -0.018610403247590064, bias: 3.2996236848399008, minimal_lwe_dimension: 606 }),
-]
+];
