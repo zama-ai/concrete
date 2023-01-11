efd0cc529c0885b2ce45e30c461013e919b8402d
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2023-01-11 20:32:32 +0100

    chore: update generated curves

--- concrete-security-curves-rust/src/curves.gen.rs
+++ concrete-security-curves-rust/src/curves.gen.rs
@@ -1,6 +1,6 @@
 const SECURITY_WEIGHTS_ARRAY: [(u64, SecurityWeights); 4] = [
-    (80, SecurityWeights { slope: -0.0404263311936459, bias: 1.660978864143658, minimal_lwe_dimension: 450 }),
-    (112, SecurityWeights { slope: -0.02967013708113588, bias: 2.16246371408387, minimal_lwe_dimension: 450 }),
-    (128, SecurityWeights { slope: -0.026405028765226296, bias: 2.482642269104389, minimal_lwe_dimension: 450 }),
-    (192, SecurityWeights { slope: -0.018610403247590064, bias: 3.2996236848399008, minimal_lwe_dimension: 606 }),
+    (80, SecurityWeights { slope: -0.04045822621883835, bias: 1.7183812000404686, minimal_lwe_dimension: 450 }),
+    (112, SecurityWeights { slope: -0.029881371645803536, bias: 2.6539316216894946, minimal_lwe_dimension: 450 }),
+    (128, SecurityWeights { slope: -0.026599462343105267, bias: 2.981543184145991, minimal_lwe_dimension: 450 }),
+    (192, SecurityWeights { slope: -0.018894148763647572, bias: 4.2700349965659115, minimal_lwe_dimension: 532 }),
 ];
