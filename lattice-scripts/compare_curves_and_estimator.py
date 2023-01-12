a12c47080ad33844a002c5c7c985acc50e77ec40
Ben <ben.curtis@zama.ai>
2023-01-12 16:13:23 +0000

    match calls

--- lattice-scripts/compare_curves_and_estimator.py
+++ lattice-scripts/compare_curves_and_estimator.py
@@ -35,7 +35,7 @@ def estimate_security_with_lattice_estimator(lwe_dimension, std_dev, log_q):
     params = LWE.Parameters(
         n=lwe_dimension, q=2 ** log_q, Xs=ND.DiscreteGaussian(0.50, -0.50), Xe=ND.DiscreteGaussian(std_dev), m=oo, tag="params"
     )
-    costs = estimate(params)
+    costs = estimate(params, red_cost_model = RC.BDGL16)
     return get_security_level(costs, 2)
 
 def get_minimal_lwe_dimension(curve, security_level, log_q):
