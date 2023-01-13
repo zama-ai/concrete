b14d11549f3f7451aa4e9144c0b33a3852bc86e4
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2023-01-13 15:17:33 +0100

    fix: cpp code generation

--- concrete-security-curves-cpp/gen_header.py
+++ concrete-security-curves-cpp/gen_header.py
@@ -7,6 +7,6 @@ def print_cpp_curves_declaration(datas):
     print("std::vector<SecurityCurve> curves {")
     for data in datas:
         print_curve(data)
-    print("}\n")
+    print("};\n")
 
 print_cpp_curves_declaration(json.load(sys.stdin))
