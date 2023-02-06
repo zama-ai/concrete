d0ae2563faec5a891deb98c77353f139a70e7484
youben11 <ayoub.benaissa@zama.ai>
2023-02-06 10:50:51 +0100

    test: add chunked eint e2e tests

--- compiler/tests/end_to_end_tests/end_to_end_jit_test.h
+++ compiler/tests/end_to_end_tests/end_to_end_jit_test.h
@@ -12,20 +12,38 @@
 #include "globals.h"
 #include "tests_tools/assert.h"
 
+llvm::StringRef DEFAULT_func = "main";
+bool DEFAULT_useDefaultFHEConstraints = false;
+bool DEFAULT_dataflowParallelize = false;
+bool DEFAULT_loopParallelize = false;
+bool DEFAULT_batchConcreteOps = false;
+double DEFAULT_global_p_error = TEST_ERROR_RATE;
+bool DEFAULT_chunkedIntegers = false;
+unsigned int DEFAULT_chunkSize = 4;
+unsigned int DEFAULT_chunkWidth = 2;
+
 // Jit-compiles the function specified by `func` from `src` and
 // returns the corresponding lambda. Any compilation errors are caught
 // and reult in abnormal termination.
 inline llvm::Expected<
     mlir::concretelang::ClientServer<mlir::concretelang::JITSupport>>
-internalCheckedJit(llvm::StringRef src, llvm::StringRef func = "main",
-                   bool useDefaultFHEConstraints = false,
-                   bool dataflowParallelize = false,
-                   bool loopParallelize = false, bool batchConcreteOps = false,
-                   double global_p_error = TEST_ERROR_RATE) {
+internalCheckedJit(
+    llvm::StringRef src, llvm::StringRef func = DEFAULT_func,
+    bool useDefaultFHEConstraints = DEFAULT_useDefaultFHEConstraints,
+    bool dataflowParallelize = DEFAULT_dataflowParallelize,
+    bool loopParallelize = DEFAULT_loopParallelize,
+    bool batchConcreteOps = DEFAULT_batchConcreteOps,
+    double global_p_error = DEFAULT_global_p_error,
+    bool chunkedIntegers = DEFAULT_chunkedIntegers,
+    unsigned int chunkSize = DEFAULT_chunkSize,
+    unsigned int chunkWidth = DEFAULT_chunkWidth) {
 
   auto options =
       mlir::concretelang::CompilationOptions(std::string(func.data()));
   options.optimizerConfig.global_p_error = global_p_error;
+  options.chunkIntegers = chunkedIntegers;
+  options.chunkSize = chunkSize;
+  options.chunkWidth = chunkWidth;
   if (useDefaultFHEConstraints) {
     options.v0FHEConstraints = defaultV0Constraints;
     options.optimizerConfig.strategy_v0 = true;
