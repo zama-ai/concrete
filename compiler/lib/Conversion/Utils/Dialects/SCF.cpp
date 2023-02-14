zama-ai/concrete

commit 73fd6c5fe751c502bcc4c7c68da4a79b753fa791

refactor(compiler): FHE to TFHE: Use OpConversionPattern for dialect conversion
Use `OpConversionPattern` instead of `OpRewritePattern` for operation
conversion during dialect conversion. This makes explicit and in-place
type conversions unnecessary, since `OpConversionPattern` already
properly converts operand types and provides them to the rewrite rule
through an operation adaptor.

The main contributions of this commit are the two class templates
`TypeConvertingReinstantiationPattern` and
`GenericOneToOneOpConversionPattern`.

The former allows for the definition of a simple replacement rule that
re-instantiates an operation after the types of its operands have been
converted. This is especially useful for type-polymorphic operations
during dialect conversion.

The latter allows for the definition of patterns, where one operation
needs to be replaced with a different operation after conversion of
its operands.

The default implementations for the class templates provide
conversions rules for operations that have a generic builder method
that takes the desired return type(s), the operands and (optionally) a
set of attributes. How attributes are discarded during a conversion
(either by omitting the builder argument or by passing an empty set of
attributes) can be defined through specialization of
`ReinstantiationAttributeDismissalStrategy`.

Custom replacement rules that deviate from the scheme above should be
implemented by specializing
`TypeConvertingReinstantiationPattern::matchAndRewrite()` and
`GenericOneToOneOpConversionPattern::matchAndRewrite()`.

compiler/lib/Conversion/Utils/Dialects/SCF.cpp
@@ -1,0 +1,40 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#include "concretelang/Conversion/Utils/Dialects/SCF.h"
+#include "mlir/Transforms/RegionUtils.h"
+#include <mlir/IR/BlockAndValueMapping.h>
+
+namespace mlir {
+namespace concretelang {
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<scf::ForOp, false>::matchAndRewrite(
+    scf::ForOp oldOp, mlir::OpConversionPattern<scf::ForOp>::OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const {
+  // Create new for loop with empty body, but converted iter args
+  scf::ForOp newForOp = rewriter.replaceOpWithNewOp<scf::ForOp>(
+      oldOp, adaptor.getLowerBound(), adaptor.getUpperBound(),
+      adaptor.getStep(), adaptor.getInitArgs(),
+      [&](OpBuilder &builder, Location loc, Value iv, ValueRange args) {});
+
+  // Move operations from old for op to new one
+  auto &newOperations = newForOp.getBody()->getOperations();
+  mlir::Block *oldBody = oldOp.getBody();
+
+  newOperations.splice(newOperations.begin(), oldBody->getOperations(),
+                       oldBody->begin(), oldBody->end());
+
+  // Remap iter args and IV
+  for (auto argsPair : llvm::zip(oldOp.getBody()->getArguments(),
+                                 newForOp.getBody()->getArguments())) {
+    replaceAllUsesInRegionWith(std::get<0>(argsPair), std::get<1>(argsPair),
+                               newForOp.getRegion());
+  }
+
+  return mlir::success();
+}
+} // namespace concretelang
+} // namespace mlir

compiler/include/concretelang/Conversion/Utils/Dialects/SCF.h
@@ -1,0 +1,29 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_CONVERSION_UTILS_DIALECTS_SCF_H_
+#define CONCRETELANG_CONVERSION_UTILS_DIALECTS_SCF_H_
+
+#include "concretelang/Conversion/Utils/ReinstantiatingOpTypeConversion.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+
+namespace mlir {
+namespace concretelang {
+
+//
+// Specializations for ForOp
+//
+
+// Specialization copying attributes omitted
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<scf::ForOp, false>::matchAndRewrite(
+    scf::ForOp oldOp, mlir::OpConversionPattern<scf::ForOp>::OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const;
+
+} // namespace concretelang
+} // namespace mlir
+
+#endif

compiler/include/concretelang/Conversion/Utils/Dialects/Tensor.h
@@ -1,0 +1,70 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_CONVERSION_UTILS_DIALECTS_TENSOR_H_
+#define CONCRETELANG_CONVERSION_UTILS_DIALECTS_TENSOR_H_
+
+#include "concretelang/Conversion/Utils/ReinstantiatingOpTypeConversion.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+
+namespace mlir {
+namespace concretelang {
+
+//
+// Specializations for CollapseShapeOp
+//
+
+// Specialization copying attributes not necessary, as the base
+// template works correctly
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<tensor::CollapseShapeOp, false>::
+    matchAndRewrite(
+        tensor::CollapseShapeOp oldOp,
+        mlir::OpConversionPattern<tensor::CollapseShapeOp>::OpAdaptor adaptor,
+        mlir::ConversionPatternRewriter &rewriter) const;
+//
+// Specializations for FromElementsOp
+//
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<mlir::tensor::FromElementsOp, false>::
+    matchAndRewrite(
+        tensor::FromElementsOp oldOp,
+        mlir::OpConversionPattern<mlir::tensor::FromElementsOp>::OpAdaptor
+            adaptor,
+        mlir::ConversionPatternRewriter &rewriter) const;
+
+//
+// Specializations for ExpandShapeOp
+//
+
+// Specialization copying attributes not necessary, as the base
+// template works correctly
+
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<tensor::ExpandShapeOp, false>::
+    matchAndRewrite(
+        tensor::ExpandShapeOp oldOp,
+        mlir::OpConversionPattern<tensor::ExpandShapeOp>::OpAdaptor adaptor,
+        mlir::ConversionPatternRewriter &rewriter) const;
+
+//
+// Specializations for GenerateOp
+//
+
+// Specialization NOT copying attributes omitted
+template <>
+mlir::LogicalResult
+TypeConvertingReinstantiationPattern<tensor::GenerateOp, true>::matchAndRewrite(
+    tensor::GenerateOp oldOp,
+    mlir::OpConversionPattern<tensor::GenerateOp>::OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const;
+
+} // namespace concretelang
+} // namespace mlir
+
+#endif // CONCRETELANG_CONVERSION_UTILS_DIALECTS_TENSOR_H_

compiler/include/concretelang/Conversion/Utils/GenericOpTypeConversionPattern.h
@@ -96,16 +96,6 @@ struct GenericTypeAndOpConverterPattern : public mlir::OpRewritePattern<OldOp> {
 private:
   mlir::TypeConverter &converter;
 };
-
-template <typename Op>
-void addDynamicallyLegalTypeOp(mlir::ConversionTarget &target,
-                               mlir::TypeConverter &typeConverter) {
-  target.addDynamicallyLegalOp<Op>([&](Op op) {
-    return typeConverter.isLegal(op->getOperandTypes()) &&
-           typeConverter.isLegal(op->getResultTypes());
-  });
-}
-
 } // namespace concretelang
 } // namespace mlir
 

compiler/include/concretelang/Conversion/Utils/Legality.h
@@ -1,0 +1,26 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_CONVERSION_UTILS_LEGALITY_H_
+#define CONCRETELANG_CONVERSION_UTILS_LEGALITY_H_
+
+#include <mlir/Transforms/DialectConversion.h>
+
+namespace mlir {
+namespace concretelang {
+
+template <typename Op>
+void addDynamicallyLegalTypeOp(mlir::ConversionTarget &target,
+                               mlir::TypeConverter &typeConverter) {
+  target.addDynamicallyLegalOp<Op>([&](Op op) {
+    return typeConverter.isLegal(op->getOperandTypes()) &&
+           typeConverter.isLegal(op->getResultTypes());
+  });
+}
+
+} // namespace concretelang
+} // namespace mlir
+
+#endif

compiler/include/concretelang/Conversion/Utils/ReinstantiatingOpTypeConversion.h
@@ -1,0 +1,216 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_CONVERSION_UTILS_REINSTANTIATINGOPTYPECONVERSION_H_
+#define CONCRETELANG_CONVERSION_UTILS_REINSTANTIATINGOPTYPECONVERSION_H_
+
+#include <mlir/Transforms/DialectConversion.h>
+
+namespace mlir {
+namespace concretelang {
+
+// Set of types defining how attributes should be handled when
+// invocating the build method of an operation upon reinstantiation
+struct ReinstantiationAttributeHandling {
+  // Copy attributes
+  struct copy {};
+
+  // Completely dismiss attributes by not passing a set of arguments
+  // to the builder at all
+  struct dismiss {};
+
+  // Dismiss attributes by passing an empty set of arguments to the
+  // builder
+  struct pass_empty_vector {};
+};
+
+// Template defining how attributes should be dismissed when invoking
+// the build method of an operation upon reinstantiation. In the
+// default case, the argument for attributes is simply dismissed.
+template <typename T> struct ReinstantiationAttributeDismissalStrategy {
+  typedef ReinstantiationAttributeHandling::dismiss strategy;
+};
+
+// Template defining how attributes should be copied when invoking the
+// build method of an operation upon reinstantiation. In the default
+// case, the argument for attributes is forwarded to the build method.
+template <typename T> struct ReinstantiationAttributeCopyStrategy {
+  typedef ReinstantiationAttributeHandling::copy strategy;
+};
+
+namespace {
+// Class template that defines the attribute handling strategy for
+// either dismissal of attributes (if `copyAttrsSwitch` is `false`) or copying
+// attributes (if `copyAttrsSwitch` is `true`).
+template <typename T, bool copyAttrsSwitch> struct AttributeHandlingSwitch {};
+
+template <typename T> struct AttributeHandlingSwitch<T, true> {
+  typedef typename ReinstantiationAttributeCopyStrategy<T>::strategy strategy;
+};
+
+template <typename T> struct AttributeHandlingSwitch<T, false> {
+  typedef
+      typename ReinstantiationAttributeDismissalStrategy<T>::strategy strategy;
+};
+
+// Simple functor-like template invoking a rewriter with a variable
+// set of arguments and an op's attributes as the last argument.
+template <typename NewOpTy, typename... Args>
+struct ReplaceOpWithNewOpCopyAttrs {
+  static NewOpTy replace(mlir::ConversionPatternRewriter &rewriter,
+                         mlir::Operation *op, mlir::TypeRange resultTypes,
+                         mlir::ValueRange operands) {
+    return rewriter.replaceOpWithNewOp<NewOpTy>(op, resultTypes, operands,
+                                                op->getAttrs());
+  }
+};
+
+// Simple functor-like template invoking a rewriter with a variable
+// set of arguments dismissing the attributes passed as the last
+// argument.
+template <typename NewOpTy, typename... Args>
+struct ReplaceOpWithNewOpDismissAttrs {
+  static NewOpTy replace(mlir::ConversionPatternRewriter &rewriter,
+                         mlir::Operation *op, mlir::TypeRange resultTypes,
+                         mlir::ValueRange operands) {
+    return rewriter.replaceOpWithNewOp<NewOpTy>(op, resultTypes, operands);
+  }
+};
+
+// Simple functor-like template invoking a rewriter with a variable
+// set of arguments dismissing the attributes by passing an empty
+// set of arguments to the builder.
+template <typename NewOpTy, typename... Args>
+struct ReplaceOpWithNewOpEmptyAttrs {
+  static NewOpTy replace(mlir::ConversionPatternRewriter &rewriter,
+                         mlir::Operation *op, mlir::TypeRange resultTypes,
+                         mlir::ValueRange operands) {
+    llvm::SmallVector<mlir::NamedAttribute> attrs{};
+    return rewriter.replaceOpWithNewOp<NewOpTy>(op, resultTypes, operands,
+                                                attrs);
+  }
+};
+
+// Functor-like template that either forwards to
+// `ReplaceOpWithNewOpCopyAttrs` or `ReplaceOpWithNewOpDismissAttrs`
+// depending on the value of `copyAttrs`.
+template <typename copyAttrsSwitch, typename OpTy, typename... Args>
+struct ReplaceOpWithNewOpAttrSwitch {};
+
+// Specialization of `ReplaceOpWithNewOpAttrSwitch` that does copy
+// attributes.
+template <typename OpTy, typename... Args>
+struct ReplaceOpWithNewOpAttrSwitch<ReinstantiationAttributeHandling::copy,
+                                    OpTy, Args...> {
+  typedef ReplaceOpWithNewOpCopyAttrs<OpTy, Args...> instantiator;
+};
+
+// Specialization of `ReplaceOpWithNewOpAttrSwitch` that does NOT copy
+// attributes by not passing attributes to the builder at all.
+template <typename OpTy, typename... Args>
+struct ReplaceOpWithNewOpAttrSwitch<ReinstantiationAttributeHandling::dismiss,
+                                    OpTy, Args...> {
+  typedef ReplaceOpWithNewOpDismissAttrs<OpTy, Args...> instantiator;
+};
+
+// Specialization of `ReplaceOpWithNewOpAttrSwitch` that does NOT copy
+// attributes by passing an empty set of attributes to the builder.
+template <typename OpTy, typename... Args>
+struct ReplaceOpWithNewOpAttrSwitch<
+    ReinstantiationAttributeHandling::pass_empty_vector, OpTy, Args...> {
+  typedef ReplaceOpWithNewOpEmptyAttrs<OpTy, Args...> instantiator;
+};
+
+} // namespace
+
+template <typename OldOp, typename NewOp, bool copyAttrs = false>
+struct GenericOneToOneOpConversionPatternBase
+    : public mlir::OpConversionPattern<OldOp> {
+  GenericOneToOneOpConversionPatternBase(mlir::MLIRContext *context,
+                                         mlir::TypeConverter &converter,
+                                         mlir::PatternBenefit benefit = 100)
+      : mlir::OpConversionPattern<OldOp>(converter, context, benefit) {}
+
+  mlir::SmallVector<mlir::Type> convertResultTypes(OldOp oldOp) const {
+    mlir::TypeConverter *converter = this->getTypeConverter();
+
+    // Convert result types
+    mlir::SmallVector<mlir::Type> resultTypes(oldOp->getNumResults());
+
+    for (unsigned i = 0; i < oldOp->getNumResults(); i++) {
+      auto result = oldOp->getResult(i);
+      resultTypes[i] = converter->convertType(result.getType());
+    }
+
+    return resultTypes;
+  }
+
+  mlir::Type convertResultType(OldOp oldOp) const {
+    mlir::TypeConverter *converter = this->getTypeConverter();
+    return converter->convertType(oldOp->getResult(0).getType());
+  }
+};
+
+// Conversion pattern that replaces an instance of an operation of the type
+// `OldOp` with an instance of the type `NewOp`, taking into account operands,
+// return types and possible copying attributes (iff copyAttrs is `true`).
+template <typename OldOp, typename NewOp, bool copyAttrs = false>
+struct GenericOneToOneOpConversionPattern
+    : public GenericOneToOneOpConversionPatternBase<OldOp, NewOp, copyAttrs> {
+  GenericOneToOneOpConversionPattern(mlir::MLIRContext *context,
+                                     mlir::TypeConverter &converter,
+                                     mlir::PatternBenefit benefit = 100)
+      : GenericOneToOneOpConversionPatternBase<OldOp, NewOp, copyAttrs>(
+            context, converter, benefit) {}
+
+  virtual mlir::LogicalResult
+  matchAndRewrite(OldOp oldOp,
+                  typename mlir::OpConversionPattern<OldOp>::OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    mlir::SmallVector<mlir::Type> resultTypes = this->convertResultTypes(oldOp);
+
+    ReplaceOpWithNewOpAttrSwitch<
+        typename AttributeHandlingSwitch<NewOp, copyAttrs>::strategy,
+        NewOp>::instantiator::replace(rewriter, oldOp,
+                                      mlir::TypeRange{resultTypes},
+                                      mlir::ValueRange{adaptor.getOperands()});
+
+    return mlir::success();
+  }
+};
+
+// Conversion pattern that retrieves the converted operands of an
+// operation of the type `Op`, converts the types of the results of
+// the operation and re-instantiates the operation type with the
+// converted operands and result types.
+template <typename Op, bool copyAttrs = false>
+struct TypeConvertingReinstantiationPattern
+    : public GenericOneToOneOpConversionPatternBase<Op, Op, copyAttrs> {
+  TypeConvertingReinstantiationPattern(mlir::MLIRContext *context,
+                                       mlir::TypeConverter &converter,
+                                       mlir::PatternBenefit benefit = 100)
+      : GenericOneToOneOpConversionPatternBase<Op, Op, copyAttrs>(
+            context, converter, benefit) {}
+  // Simple forward that makes the method specializable out of class
+  // directly for this class rather than for its base
+  virtual mlir::LogicalResult
+  matchAndRewrite(Op op,
+                  typename mlir::OpConversionPattern<Op>::OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    mlir::SmallVector<mlir::Type> resultTypes = this->convertResultTypes(op);
+
+    ReplaceOpWithNewOpAttrSwitch<
+        typename AttributeHandlingSwitch<Op, copyAttrs>::strategy,
+        Op>::instantiator::replace(rewriter, op, mlir::TypeRange{resultTypes},
+                                   mlir::ValueRange{adaptor.getOperands()});
+
+    return mlir::success();
+  }
+};
+
+} // namespace concretelang
+} // namespace mlir
+
+#endif

compiler/include/concretelang/Conversion/Utils/TensorOpTypeConversion.h
@@ -10,7 +10,8 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Transforms/DialectConversion.h"
 
-#include "concretelang/Conversion/Utils/GenericOpTypeConversionPattern.h"
+#include "concretelang/Conversion/Utils/Dialects/Tensor.h"
+#include "concretelang/Conversion/Utils/Legality.h"
 
 namespace mlir {
 namespace concretelang {
@@ -20,37 +21,43 @@ populateWithTensorTypeConverterPatterns(mlir::RewritePatternSet &patterns,
                                         mlir::ConversionTarget &target,
                                         mlir::TypeConverter &typeConverter) {
   // ExtractOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::ExtractOp>>(
+  patterns.add<TypeConvertingReinstantiationPattern<mlir::tensor::ExtractOp>>(
       patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::ExtractOp>(target, typeConverter);
+
   // ExtractSliceOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::ExtractSliceOp>>(
+  patterns.add<
+      TypeConvertingReinstantiationPattern<mlir::tensor::ExtractSliceOp, true>>(
       patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::ExtractSliceOp>(target,
                                                           typeConverter);
 
   // InsertOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::InsertOp>>(
+  patterns.add<TypeConvertingReinstantiationPattern<mlir::tensor::InsertOp>>(
       patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::InsertOp>(target, typeConverter);
   // InsertSliceOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::InsertSliceOp>>(
+  patterns.add<
+      TypeConvertingReinstantiationPattern<mlir::tensor::InsertSliceOp, true>>(
       patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::InsertSliceOp>(target, typeConverter);
 
   // FromElementsOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::FromElementsOp>>(
-      patterns.getContext(), typeConverter);
+  patterns
+      .add<TypeConvertingReinstantiationPattern<mlir::tensor::FromElementsOp>>(
+          patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::FromElementsOp>(target,
                                                           typeConverter);
   // TensorCollapseShapeOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::CollapseShapeOp>>(
-      patterns.getContext(), typeConverter);
+  patterns
+      .add<TypeConvertingReinstantiationPattern<mlir::tensor::CollapseShapeOp>>(
+          patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::CollapseShapeOp>(target,
                                                            typeConverter);
   // TensorExpandShapeOp
-  patterns.add<GenericTypeConverterPattern<mlir::tensor::ExpandShapeOp>>(
-      patterns.getContext(), typeConverter);
+  patterns
+      .add<TypeConvertingReinstantiationPattern<mlir::tensor::ExpandShapeOp>>(
+          patterns.getContext(), typeConverter);
   addDynamicallyLegalTypeOp<mlir::tensor::ExpandShapeOp>(target, typeConverter);
 }
 } // namespace concretelang

compiler/lib/Conversion/CMakeLists.txt
@@ -9,5 +9,13 @@ add_subdirectory(SDFGToStreamEmulator)
 add_subdirectory(MLIRLowerableDialectsToLLVM)
 add_subdirectory(LinalgExtras)
 add_subdirectory(ExtractSDFGOps)
+add_subdirectory(Utils)
 
-add_mlir_library(ConcretelangConversion Tools.cpp LINK_LIBS PUBLIC MLIRIR)
+add_mlir_library(
+  ConcretelangConversion
+  Tools.cpp
+  Utils/Dialects/SCF.cpp
+  Utils/Dialects/Tensor.cpp
+  LINK_LIBS
+  PUBLIC
+  MLIRIR)

commit fc02bb5cd8ece713aa596021d306118727a31602

fix(compiler): Copy attributes when reinstantiating scf.for ops
The switch to reinstantiating conversion patterns for the conversion
from FHE to TFHE in commit 73fd6c5fe751c502bcc4c7c68da4a79b753fa791
caused all attributes of `scf.for` operations to be dropped during the
conversion. This included the custom attribute `parallel`, which is
exploited further down the compilation pipeline to generate parallel
code. As a result, the performance of end-to-end benchmarks dropped
significantly.

This patch copies all attributes of `scf.for` operations upon
reinstantiation, which solves the performance regression.

compiler/lib/Conversion/Utils/Dialects/SCF.cpp
@@ -20,6 +20,8 @@ TypeConvertingReinstantiationPattern<scf::ForOp, false>::matchAndRewrite(
       adaptor.getStep(), adaptor.getInitArgs(),
       [&](OpBuilder &builder, Location loc, Value iv, ValueRange args) {});
 
+  newForOp->setAttrs(adaptor.getAttributes());
+
   // Move operations from old for op to new one
   auto &newOperations = newForOp.getBody()->getOperations();
   mlir::Block *oldBody = oldOp.getBody();