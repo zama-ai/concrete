277ad2cd3b046a0f55d0e27bd54862dc8d0790e3
Arthur Meyre <arthur.meyre@zama.ai>
2023-03-15 19:13:55 +0100

    feat(noise): add multi_bit blind rotate and external product noise function

--- src/gaussian_noise/noise.rs
+++ src/gaussian_noise/noise.rs
@@ -4,4 +4,6 @@ pub mod external_product_glwe;
 pub mod keyswitch;
 pub mod keyswitch_one_bit;
 pub mod modulus_switching;
+pub mod multi_bit_blind_rotate;
+pub mod multi_bit_external_product_glwe;
 pub mod private_packing_keyswitch;
