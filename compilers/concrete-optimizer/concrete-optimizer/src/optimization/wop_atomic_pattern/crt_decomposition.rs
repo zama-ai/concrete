585cfaf747e3a5a696400d28f7b6a056ca1200b1
Mayeul@Zama <mayeul.debellabre@zama.ai>
2023-03-22 09:28:28 +0100

    potential change

--- compilers/concrete-optimizer/concrete-optimizer/src/optimization/wop_atomic_pattern/crt_decomposition.rs
+++ compilers/concrete-optimizer/concrete-optimizer/src/optimization/wop_atomic_pattern/crt_decomposition.rs
@@ -3,21 +3,19 @@ use crate::dag::operator::Precision;
 // Default heuristic to split in several word
 pub fn default_coprimes(precision: Precision) -> Result<Vec<u64>, String> {
     Ok(match precision {
-        1 => vec![2],
-        2 => vec![4],
-        3 => vec![8],
-        4 | 5 => vec![2, 3, 7],
-        6 => vec![2, 5, 7],
-        7 => vec![3, 7, 8],
-        8 => vec![5, 7, 8],
-        9 => vec![3, 5, 7, 8],
-        10 => vec![7, 12, 13],
-        11 => vec![5, 7, 8, 9],
-        12 => vec![7, 8, 9, 11],
-        13 => vec![3, 5, 7, 8, 11],
-        14 => vec![5, 7, 8, 9, 11],
-        15 => vec![11, 13, 15, 16],
-        16 => vec![7, 8, 9, 11, 13],
+        1 => vec![2],                         // 1 bit
+        2 => vec![4],                         // 2 bit
+        3 => vec![8],                         // 3 bit
+        4 | 5 => vec![2, 3, 7],               // 1,2,3 bits
+        6 => vec![2, 5, 7],                   // 1,3,3 bits
+        7 => vec![3, 7, 8],                   // 2,3,3 bits
+        8 => vec![5, 7, 8],                   // 3,3,3 bits
+        9 => vec![5, 7, 16],                  // 3,3,4 bits
+        10 => vec![7, 15, 16],                // 3,4,4 bits
+        11 => vec![13, 15, 16],               // 4,4,4 bits
+        12 => vec![7, 13, 15, 16],            // 3,4,4,4 bits
+        13 | 14 | 15 => vec![11, 13, 15, 16], // 4,4,4,4 bits
+        16 => vec![7, 8, 9, 11, 13],          // 4,4,4,4,4 bits
         0 => return Err("Precision cannot be zero".into()),
         _ => return Err("Precision is limited to 16-bits".into()),
     })
