39b46a55eb87e6e6424d19b1a1bb9a4c29471bf1
Antoniu Pop <antoniu.pop@zama.ai>
2023-03-24 10:44:56 +0000

    feat(compiler): add SDFG op generation for batched operations.

--- compilers/concrete-compiler/compiler/include/concretelang/Runtime/stream_emulator_api.h
+++ compilers/concrete-compiler/compiler/include/concretelang/Runtime/stream_emulator_api.h
@@ -42,6 +42,27 @@ void stream_emulator_make_memref_bootstrap_lwe_u64_process(
     uint32_t poly_size, uint32_t level, uint32_t base_log, uint32_t glwe_dim,
     uint32_t output_size, uint32_t bsk_index, void *context);
 
+void stream_emulator_make_memref_batched_add_lwe_ciphertexts_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout);
+void stream_emulator_make_memref_batched_add_plaintext_lwe_ciphertext_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout);
+void stream_emulator_make_memref_batched_add_plaintext_cst_lwe_ciphertext_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout);
+void stream_emulator_make_memref_batched_mul_cleartext_lwe_ciphertext_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout);
+void stream_emulator_make_memref_batched_mul_cleartext_cst_lwe_ciphertext_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout);
+void stream_emulator_make_memref_batched_negate_lwe_ciphertext_u64_process(
+    void *dfg, void *sin1, void *sout);
+void stream_emulator_make_memref_batched_keyswitch_lwe_u64_process(
+    void *dfg, void *sin1, void *sout, uint32_t level, uint32_t base_log,
+    uint32_t input_lwe_dim, uint32_t output_lwe_dim, uint32_t output_size,
+    void *context);
+void stream_emulator_make_memref_batched_bootstrap_lwe_u64_process(
+    void *dfg, void *sin1, void *sin2, void *sout, uint32_t input_lwe_dim,
+    uint32_t poly_size, uint32_t level, uint32_t base_log, uint32_t glwe_dim,
+    uint32_t output_size, void *context);
+
 void *stream_emulator_make_uint64_stream(const char *name, stream_type stype);
 void stream_emulator_put_uint64(void *stream, uint64_t e);
 uint64_t stream_emulator_get_uint64(void *stream);
@@ -53,6 +74,18 @@ void stream_emulator_put_memref(void *stream, uint64_t *allocated,
 void stream_emulator_get_memref(void *stream, uint64_t *out_allocated,
                                 uint64_t *out_aligned, uint64_t out_offset,
                                 uint64_t out_size, uint64_t out_stride);
+
+void *stream_emulator_make_memref_batch_stream(const char *name,
+                                               stream_type stype);
+void stream_emulator_put_memref_batch(void *stream, uint64_t *allocated,
+                                      uint64_t *aligned, uint64_t offset,
+                                      uint64_t size0, uint64_t size1,
+                                      uint64_t stride0, uint64_t stride1);
+void stream_emulator_get_memref_batch(void *stream, uint64_t *out_allocated,
+                                      uint64_t *out_aligned,
+                                      uint64_t out_offset, uint64_t out_size0,
+                                      uint64_t out_size1, uint64_t out_stride0,
+                                      uint64_t out_stride1);
 }
 
 #endif
