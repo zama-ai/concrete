84a3dd7a8d7bb1424ea6aea626df4fae62128e87
Mayeul@Zama <mayeul.debellabre@zama.ai>
2023-04-06 11:52:42 +0200

    chore(backend-cpu): use zig build

---
+++ backends/concrete-cpu/implementation/test/all.zig
@@ -0,0 +1,6 @@
+comptime {
+    _ = @import("test_encryption.zig");
+    _ = @import("test_bootstrap.zig");
+    _ = @import("test_bit_extract.zig");
+    _ = @import("test_vertical_packing.zig");
+}
