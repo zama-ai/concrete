efa866f069652b9201e872c1c6e9972ba87752ee
Umut <umutsahin@protonmail.com>
2023-04-07 12:07:20 +0200

    chore(frontend/python): use smaller global p error for tests to avoid random ci failures

--- frontends/concrete-python/tests/conftest.py
+++ frontends/concrete-python/tests/conftest.py
@@ -128,7 +128,7 @@ class Helpers:
             auto_parallelize=False,
             jit=True,
             insecure_key_cache_location=INSECURE_KEY_CACHE_LOCATION,
-            global_p_error=(1 / 10_000),
+            global_p_error=(1 / 100_000),
             single_precision=(not USE_MULTI_PRECISION),
         )
 
