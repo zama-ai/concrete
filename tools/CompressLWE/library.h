599315c3d87d719da94d94845455d3e17b737bfe
Mayeul@Zama <mayeul.debellabre@zama.ai>
2023-06-05 18:10:55 +0200

    fmt compressLWE

--- tools/CompressLWE/library.h
+++ tools/CompressLWE/library.h
@@ -3,31 +3,26 @@
 
 #include "utils.h"
 
-ipcl::CipherText compressSingle(
-        const ipcl::CipherText &compressionKey,
-        const std::vector<uint64_t> &lweCt,
-        const LWEParams &params);
+ipcl::CipherText compressSingle(const ipcl::CipherText &compressionKey,
+                                const std::vector<uint64_t> &lweCt,
+                                const LWEParams &params);
 
-CompressedCiphertext compressBatched(
-        const ipcl::CipherText &compressionKey,
-        const std::vector<std::vector<uint64_t>> &cts,
-        const LWEParams &params);
+CompressedCiphertext
+compressBatched(const ipcl::CipherText &compressionKey,
+                const std::vector<std::vector<uint64_t>> &cts,
+                const LWEParams &params);
 
-uint64_t decryptCompressedSingle(
-        const ipcl::CipherText &resultCt,
-        const ipcl::PrivateKey &paiSk,
-        const LWEParams &params);
+uint64_t decryptCompressedSingle(const ipcl::CipherText &resultCt,
+                                 const ipcl::PrivateKey &paiSk,
+                                 const LWEParams &params);
 
-std::vector<uint64_t> decryptCompressedBatched(
-        const CompressedCiphertext &compressedCiphertext,
-        const ipcl::PrivateKey &paiSk,
-        const LWEParams &params,
-        uint64_t ciphers);
+std::vector<uint64_t>
+decryptCompressedBatched(const CompressedCiphertext &compressedCiphertext,
+                         const ipcl::PrivateKey &paiSk, const LWEParams &params,
+                         uint64_t ciphers);
 
 Keys generateKeys(const std::vector<uint64_t> &lweKey);
 
-void finalizeDecryption(
-        BigNumber &res,
-        const LWEParams &params);
+void finalizeDecryption(BigNumber &res, const LWEParams &params);
 
-#endif //LIBRARY_H
+#endif // LIBRARY_H
--- tools/CompressLWE/tests.cpp
+++ tools/CompressLWE/tests.cpp
@@ -1,145 +1,144 @@
-#include <iostream>
-#include <cassert>
-#include "library.h"
 #include "chrono"
+#include "library.h"
+#include <cassert>
+#include <iostream>
 using namespace std::chrono;
 void test_single_lwe_compress() {
 
-    uint64_t n = 630;
-    uint64_t log_q = 64;
-    uint64_t p = 32;
+  uint64_t n = 630;
+  uint64_t log_q = 64;
+  uint64_t p = 32;
 
-    // Generate a random vector<uint64_t> of size n+1 with values in [0, q) and call it the secret key
-    std::vector<uint64_t> lwe_key(n);
-    for (int i = 0; i < n; i++) {
-        lwe_key[i] = sample(log_q);
-    }
+  // Generate a random vector<uint64_t> of size n+1 with values in [0, q) and
+  // call it the secret key
+  std::vector<uint64_t> lwe_key(n);
+  for (int i = 0; i < n; i++) {
+    lwe_key[i] = sample(log_q);
+  }
 
-    // Generate a ciphertext
-    std::vector<uint64_t> lwe_ct(n + 1);
-    for (int i = 0; i < n + 1; i++) {
-        lwe_ct[i] = sample(log_q);
-    }
+  // Generate a ciphertext
+  std::vector<uint64_t> lwe_ct(n + 1);
+  for (int i = 0; i < n + 1; i++) {
+    lwe_ct[i] = sample(log_q);
+  }
 
-    // Call the generate Keys function with the lwe_key
-    auto key = generateKeys(lwe_key);
+  // Call the generate Keys function with the lwe_key
+  auto key = generateKeys(lwe_key);
 
-    // Call the compressSingle function with the compression key, the ciphertext, and the LWEParams
-    LWEParams params(n, log_q, p);
-    auto compressed_ct = compressSingle(key.compKey, lwe_ct, params);
+  // Call the compressSingle function with the compression key, the ciphertext,
+  // and the LWEParams
+  LWEParams params(n, log_q, p);
+  auto compressed_ct = compressSingle(key.compKey, lwe_ct, params);
 
-    // Call the decryptCompressedSingle function with the compressed ciphertext, the paillier private key, and the LWEParams
-    auto decrypted = decryptCompressedSingle(compressed_ct, key.paiKeys.priv_key, params);
+  // Call the decryptCompressedSingle function with the compressed ciphertext,
+  // the paillier private key, and the LWEParams
+  auto decrypted =
+      decryptCompressedSingle(compressed_ct, key.paiKeys.priv_key, params);
 
-    uint64_t lwe_decrypted = decryptLWE(lwe_ct, lwe_key, params);
-    assert(decrypted == lwe_decrypted);
+  uint64_t lwe_decrypted = decryptLWE(lwe_ct, lwe_key, params);
+  assert(decrypted == lwe_decrypted);
 
-    // print success message
-    std::cout << "Single Test Passed!" << std::endl;
-
+  // print success message
+  std::cout << "Single Test Passed!" << std::endl;
 }
 
 void test_batched_lwe_compress() {
-    uint64_t n = 630;
-    uint64_t log_q = 64;
-    uint64_t p = 32;
+  uint64_t n = 630;
+  uint64_t log_q = 64;
+  uint64_t p = 32;
 
-    assert(log_q <= 64);
+  assert(log_q <= 64);
 
-    // Get starting timepoint
-    auto start = high_resolution_clock::now();
-    // Generate a lwe key of size n with values in [0, q)
-    std::vector<uint64_t> lwe_key(n);
-    for (int i = 0; i < n; i++) {
-        lwe_key[i] = sample(log_q);
+  // Get starting timepoint
+  auto start = high_resolution_clock::now();
+  // Generate a lwe key of size n with values in [0, q)
+  std::vector<uint64_t> lwe_key(n);
+  for (int i = 0; i < n; i++) {
+    lwe_key[i] = sample(log_q);
+  }
+  // Get ending timepoint
+  auto stop = high_resolution_clock::now();
+  // Get duration. Substart timepoints to
+  auto d_lwe_key = duration_cast<microseconds>(stop - start);
+
+  start = stop;
+
+  uint64_t num_cts = 784;
+  // Generate a batch of lwe ciphertexts
+  std::vector<std::vector<uint64_t>> lwe_cts(num_cts);
+  for (int i = 0; i < num_cts; i++) {
+    lwe_cts[i] = std::vector<uint64_t>(n + 1);
+    for (int j = 0; j < n + 1; j++) {
+      lwe_cts[i][j] = sample(log_q);
     }
-    // Get ending timepoint
-    auto stop = high_resolution_clock::now();
-    // Get duration. Substart timepoints to
-    auto d_lwe_key = duration_cast<microseconds>(stop - start);
+  }
+  // Get ending timepoint
+  stop = high_resolution_clock::now();
+  // Get duration. Substart timepoints to
+  auto d_lwe_cts = duration_cast<microseconds>(stop - start);
 
-    start=stop;
+  start = stop;
 
-    uint64_t num_cts = 784;
-    // Generate a batch of lwe ciphertexts
-    std::vector<std::vector<uint64_t>> lwe_cts(num_cts);
-    for (int i = 0; i < num_cts; i++) {
-        lwe_cts[i] = std::vector<uint64_t>(n + 1);
-        for (int j = 0; j < n + 1; j++) {
-            lwe_cts[i][j] = sample(log_q);
-        }
-    }
-    // Get ending timepoint
-    stop = high_resolution_clock::now();
-    // Get duration. Substart timepoints to
-    auto d_lwe_cts = duration_cast<microseconds>(stop - start);
+  auto key = generateKeys(lwe_key);
 
-    start = stop;
+  // Get ending timepoint
+  stop = high_resolution_clock::now();
+  // Get duration. Substart timepoints to
+  auto d_keygen = duration_cast<microseconds>(stop - start);
 
-    auto key = generateKeys(lwe_key);
+  start = stop;
 
-    // Get ending timepoint
-    stop = high_resolution_clock::now();
-    // Get duration. Substart timepoints to
-    auto d_keygen = duration_cast<microseconds>(stop - start);
+  LWEParams params(n, log_q, p);
 
-    start=stop;
+  CompressedCiphertext compressed_ct =
+      compressBatched(key.compKey, lwe_cts, params);
 
-    LWEParams params(n, log_q, p);
+  // Get ending timepoint
+  stop = high_resolution_clock::now();
+  // Get duration. Substart timepoints to
+  auto d_compress = duration_cast<microseconds>(stop - start);
 
-    CompressedCiphertext compressed_ct = compressBatched(
-            key.compKey,
-            lwe_cts,
-            params
-    );
+  start = stop;
 
-    // Get ending timepoint
-    stop = high_resolution_clock::now();
-    // Get duration. Substart timepoints to
-    auto d_compress = duration_cast<microseconds>(stop - start);
+  std::vector<uint64_t> decrypted = decryptCompressedBatched(
+      compressed_ct, key.paiKeys.priv_key, params, num_cts);
 
-    start = stop;
+  // Get ending timepoint
+  stop = high_resolution_clock::now();
+  // Get duration. Substart timepoints to
+  auto d_decrypt = duration_cast<microseconds>(stop - start);
 
-    std::vector<uint64_t> decrypted = decryptCompressedBatched(
-            compressed_ct,
-            key.paiKeys.priv_key,
-            params,
-            num_cts
-    );
-
-    // Get ending timepoint
-    stop = high_resolution_clock::now();
-    // Get duration. Substart timepoints to
-    auto d_decrypt = duration_cast<microseconds>(stop - start);
-
-    for (int i = 0; i < num_cts; i++) {
-        uint64_t lwe_decrypted = decryptLWE(lwe_cts[i], lwe_key, params);
-        assert(decrypted[i] == lwe_decrypted);
-    }
-    std::cout << "Batched Test Passed!" << std::endl;
-    std::cout << "Time taken by LWE key:  " << d_lwe_key.count() << " microseconds" << std::endl;
-    std::cout << "Time taken by LWE cts:  " << d_lwe_cts.count() << " microseconds" << std::endl;
-    std::cout << "Time taken by keygen:   " << d_keygen.count() << " microseconds" << std::endl;
-    std::cout << "Time taken by compress: " << d_compress.count() << " microseconds" << std::endl;
-    std::cout << "Time taken by decrypt:  " << d_decrypt.count() << " microseconds" << std::endl;
+  for (int i = 0; i < num_cts; i++) {
+    uint64_t lwe_decrypted = decryptLWE(lwe_cts[i], lwe_key, params);
+    assert(decrypted[i] == lwe_decrypted);
+  }
+  std::cout << "Batched Test Passed!" << std::endl;
+  std::cout << "Time taken by LWE key:  " << d_lwe_key.count()
+            << " microseconds" << std::endl;
+  std::cout << "Time taken by LWE cts:  " << d_lwe_cts.count()
+            << " microseconds" << std::endl;
+  std::cout << "Time taken by keygen:   " << d_keygen.count() << " microseconds"
+            << std::endl;
+  std::cout << "Time taken by compress: " << d_compress.count()
+            << " microseconds" << std::endl;
+  std::cout << "Time taken by decrypt:  " << d_decrypt.count()
+            << " microseconds" << std::endl;
 }
-
 
 void test_conversion() {
-    for (uint64_t i = 0; i < 10; i++) {
-        uint64_t x = sample(64);
-        BigNumber x_bn = from64(x);
-        uint64_t y = to64(x_bn);
-        assert(x == y);
-    }
-    std::cout << "Conversion Test Passed!" << std::endl;
+  for (uint64_t i = 0; i < 10; i++) {
+    uint64_t x = sample(64);
+    BigNumber x_bn = from64(x);
+    uint64_t y = to64(x_bn);
+    assert(x == y);
+  }
+  std::cout << "Conversion Test Passed!" << std::endl;
 }
 
-
 int main() {
-    srand(time(nullptr));
-    test_conversion();
-    test_single_lwe_compress();
-    test_batched_lwe_compress();
-    return 0;
+  srand(time(nullptr));
+  test_conversion();
+  test_single_lwe_compress();
+  test_batched_lwe_compress();
+  return 0;
 }
