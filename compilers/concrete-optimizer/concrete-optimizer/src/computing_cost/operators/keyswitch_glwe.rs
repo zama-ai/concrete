588c6adfd757e596b56b41fe1f374dcf24e55a60
Charlotte.Bonte <charlotte.bonte@zama.ai>
2023-06-28 10:31:45 +0200

    fix complexity glwe keyswitch

--- compilers/concrete-optimizer/concrete-optimizer/src/computing_cost/operators/keyswitch_glwe.rs
+++ compilers/concrete-optimizer/concrete-optimizer/src/computing_cost/operators/keyswitch_glwe.rs
@@ -15,11 +15,12 @@ impl KsGlweComplexity {
         let input_glwe_dimension = params.input_glwe_params.glwe_dimension as i64;
         let output_glwe_dimension = params.output_glwe_params.glwe_dimension as i64;
         let level = params.ks_decomposition_parameter.level as i64;
+        let poly_dim = 2_f64.powi(params.input_glwe_params.log2_polynomial_size as i32);
 
         let output_glwe_size = output_glwe_dimension + 1;
-        let count_decomposition = input_glwe_dimension * level;
-        let count_mul = input_glwe_dimension * level * output_glwe_size;
-        let count_add = (input_glwe_dimension * level - 1) * output_glwe_size + 1;
+        let count_decomposition = input_glwe_dimension * level * poly_dim;
+        let count_mul = input_glwe_dimension * level * output_glwe_size * poly_dim;
+        let count_add = (input_glwe_dimension * level * poly_dim - 1) * output_glwe_size + 1;
         (count_decomposition + count_mul + count_add) as Complexity
     }
 }
