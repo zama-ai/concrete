801b59290da34887c1ca1ba3ce4e6cd26ad5b36a
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2023-08-08 11:47:42 +0200

    test(frontend): add a --patch option to ML tests
    to test the removal of workarounds, or the use of new Concrete features
    
    closes #442

--- ci/scripts/test_cml.sh
+++ ci/scripts/test_cml.sh
@@ -12,13 +12,14 @@ function usage() {
     echo "About ML repository:"
     echo "--ml_branch <branch>              Specify the branch of ML repo (default is main)"
     echo
+    echo "About patches to apply to ML sources/tests, to test the removal of workarounds:"
+    echo "--patch patch_diff_file           Specify a local patch to apply. Patches are obtained by"
+    echo "                                  modifying ML files, and then `git diff > patch.diff`"
+    echo
     echo "About CP version:"
     echo "--cp_version x.y.z                Install the chosen version of CP"
     echo "--last_nightly_cp_version         Install the last nightly of CP"
     echo "--use-wheel /path/to/wheel        Install CP from this wheel"
-    echo
-    echo "About the tests to run"
-    echo "--use_cml_command_line            Use exactly commandline by ML team"
     echo
     echo "And the other options"
     echo "--quick_debug_of_the_script       Do not use, unless you debug the script"
@@ -52,6 +53,8 @@ ML_BRANCH="main"
 # Specify wheel's path if we want to install CP from wheel
 WHEEL=""
 
+PATCH=""
+
 while [ -n "$1" ]
 do
    case "$1" in
@@ -68,6 +71,11 @@ do
             shift
             WHEEL="${1}"
             CP_VERSION="wheel"
+            ;;
+
+        "--patch" )
+            shift
+            PATCH="${1}"
             ;;
 
         "--quick_debug_of_the_script" )
@@ -101,6 +109,12 @@ do
    esac
    shift
 done
+
+# The patch is assumed to be local
+if [ "$PATCH" != "" ]
+then
+    PATCH="$PWD/$PATCH"
+fi
 
 # Directory for tests
 echo
@@ -178,6 +192,14 @@ fi
 INSTALLED_CP=`pip freeze | grep "concrete-python"`
 echo
 echo "Installed Concrete-Python: ${INSTALLED_CP}"
+
+if [ "$PATCH" != "" ]
+then
+    echo "Applying patch $PATCH"
+    git apply $PATCH
+    echo "Modifications in ML repository:"
+    git diff
+fi
 
 # Launch CML tests with pytest (and ignore flaky ones)
 # As compared to regular `make pytest`, known flaky errors from Concrete ML are simply ignored
