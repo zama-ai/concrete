1ea29d860f1cb174406b5e67f38e66363b827de7
rudy <rudy.sicard@gmail.com>
2023-09-08 11:32:33 +0200

    fix(compiler): ci, format_cpp.sh, bug + better message

--- compilers/concrete-compiler/compiler/scripts/format_cpp.sh
+++ compilers/concrete-compiler/compiler/scripts/format_cpp.sh
@@ -7,6 +7,11 @@ EXCLUDE_DIRS="-path ./include/boost-single-header -prune -o"
 find ./{include,lib,src,tests} $EXCLUDE_DIRS -iregex '^.*\.\(cpp\|cc\|h\|hpp\)$' -print | xargs clang-format -i -style='file'
 
 # show changes if any
-git --no-pager diff
-# fail if there is a diff, success otherwise
-git diff | ifne exit 1
+git --no-pager diff --patience
+
+# success if the diff is empty
+git --no-pager diff --exit-code && exit 0
+
+echo
+echo "Formatting issue: Please run 'scripts/format_cpp.sh' in compilers/concrete-compiler/compiler and 'git add -p'"
+exit 1
