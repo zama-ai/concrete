e003a9f0318247d7f71d752b04583806c150e7d3
rudy <rudy.sicard@gmail.com>
2023-10-30 15:25:28 +0100

    FORCETEST

--- frontends/concrete-python/concrete/__init__.py
+++ frontends/concrete-python/concrete/__init__.py
@@ -5,3 +5,5 @@ Setup concrete namespace.
 # Do not modify, this is to have a compatible namespace package
 # https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#pkg-resources-style-namespace-packages
 __import__("pkg_resources").declare_namespace(__name__)  # pragma: no cover
+
+# FORCE RETESTS
