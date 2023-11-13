270131f2b2c1b54164b2293197e6c834d8238908
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2023-11-13 16:06:49 +0100

    test(compiler): Add retry for fixing some flaky tests

--- compilers/concrete-compiler/compiler/tests/end_to_end_tests/end_to_end_test.cc
+++ compilers/concrete-compiler/compiler/tests/end_to_end_tests/end_to_end_test.cc
@@ -25,9 +25,11 @@ class EndToEndTest : public ::testing::Test {
 public:
   explicit EndToEndTest(std::string program, TestDescription desc,
                         std::optional<TestErrorRate> errorRate,
-                        mlir::concretelang::CompilationOptions options)
+                        mlir::concretelang::CompilationOptions options,
+                        int retryFailingTests)
       : program(program), desc(desc), errorRate(errorRate),
-        testCircuit(std::nullopt), options(options) {
+        testCircuit(std::nullopt), options(options),
+        retryFailingTests(retryFailingTests) {
     if (errorRate.has_value()) {
       options.optimizerConfig.global_p_error = errorRate->global_p_error;
       options.optimizerConfig.p_error = errorRate->global_p_error;
@@ -43,19 +45,19 @@ public:
     ce.setCompilationOptions(options);
     auto expectCompilationResult = ce.compile({program}, artifactFolder);
     ASSERT_EXPECTED_SUCCESS(expectCompilationResult);
-    auto compiled = expectCompilationResult.get();
+    library = expectCompilationResult.get();
 
     /* Retrieve the keyset */
     auto keyset =
         getTestKeySetCachePtr()
-            ->getKeyset(compiled.getProgramInfo().asReader().getKeyset(), 0, 0)
+            ->getKeyset(library->getProgramInfo().asReader().getKeyset(), 0, 0)
             .value();
 
     /* Create the test circuit */
     testCircuit =
         TestCircuit::create(
-            keyset, compiled.getProgramInfo().asReader(),
-            compiled.getSharedLibraryPath(compiled.getOutputDirPath()), 0, 0,
+            keyset, library->getProgramInfo().asReader(),
+            library->getSharedLibraryPath(library->getOutputDirPath()), 0, 0,
             false)
             .value();
 
@@ -77,14 +79,31 @@ public:
   }
 
   void testOnce() {
-    // We execute the circuit.
-    auto maybeRes = (*testCircuit).call(args);
-    ASSERT_OUTCOME_HAS_VALUE(maybeRes);
-    auto result = maybeRes.value();
+    for (auto tests_rep = 0; tests_rep <= retryFailingTests; tests_rep++) {
+      // We execute the circuit.
+      auto maybeRes = (*testCircuit).call(args);
+      ASSERT_OUTCOME_HAS_VALUE(maybeRes);
+      auto result = maybeRes.value();
 
-    /* Check result */
-    for (size_t i = 0; i < desc.outputs.size(); i++) {
-      ASSERT_LLVM_ERROR(checkResult(desc.outputs[i], result[i]));
+      /* Check result */
+      for (size_t i = 0; i < desc.outputs.size(); i++) {
+        auto maybeErr = checkResult(desc.outputs[i], result[i]);
+        if (!maybeErr)
+          return;
+        if (tests_rep < retryFailingTests) {
+          llvm::errs() << "/!\\ WARNING RETRY TEST: " << maybeErr << "\n";
+          llvm::consumeError(std::move(maybeErr));
+          llvm::errs() << "Regenerating keyset\n";
+          __uint128_t seed = 0;
+          auto csprng = ConcreteCSPRNG(seed);
+          Keyset keyset =
+              Keyset(library->getProgramInfo().asReader().getKeyset(), csprng);
+          testCircuit->setKeySet(keyset);
+          break;
+        } else {
+          ASSERT_LLVM_ERROR(std::move(maybeErr));
+        }
+      }
     }
   }
 
@@ -117,8 +136,10 @@ private:
   std::string artifactFolder;
   TestDescription desc;
   std::optional<TestErrorRate> errorRate;
+  std::optional<mlir::concretelang::CompilerEngine::Library> library;
   std::optional<TestCircuit> testCircuit;
   mlir::concretelang::CompilationOptions options;
+  int retryFailingTests;
   std::vector<Value> args;
 };
 
@@ -134,19 +155,22 @@ void registerEndToEnd(std::string suiteName, std::string testName,
                       std::string valueName, std::string program,
                       TestDescription test,
                       std::optional<TestErrorRate> errorRate,
-                      mlir::concretelang::CompilationOptions options) {
+                      mlir::concretelang::CompilationOptions options,
+                      int retryFailingTests) {
   // TODO: Get file and line from yaml
   auto file = __FILE__;
   auto line = __LINE__;
-  ::testing::RegisterTest(
-      suiteName.c_str(), testName.c_str(), nullptr, valueName.c_str(), file,
-      line, [=]() -> EndToEndTest * {
-        return new EndToEndTest(program, test, errorRate, options);
-      });
+  ::testing::RegisterTest(suiteName.c_str(), testName.c_str(), nullptr,
+                          valueName.c_str(), file, line,
+                          [=]() -> EndToEndTest * {
+                            return new EndToEndTest(program, test, errorRate,
+                                                    options, retryFailingTests);
+                          });
 }
 
 void registerEndToEnd(std::string suiteName, EndToEndDesc desc,
-                      mlir::concretelang::CompilationOptions options) {
+                      mlir::concretelang::CompilationOptions options,
+                      int retryFailingTests) {
   if (desc.v0Constraint.has_value()) {
     options.v0FHEConstraints = desc.v0Constraint;
   }
@@ -161,13 +185,13 @@ void registerEndToEnd(std::string suiteName, EndToEndDesc desc,
     auto testName = getTestName(desc, options, i);
     if (desc.test_error_rates.empty()) {
       registerEndToEnd(suiteName, testName, valueName, desc.program, test,
-                       std::nullopt, options);
+                       std::nullopt, options, retryFailingTests);
     } else {
       auto j = 0;
       for (auto rate : desc.test_error_rates) {
         auto rateName = testName + "_rate" + std::to_string(j);
         registerEndToEnd(suiteName, rateName, valueName, desc.program, test,
-                         rate, options);
+                         rate, options, retryFailingTests);
         j++;
       }
     }
@@ -181,9 +205,10 @@ void registerEndToEnd(std::string suiteName, EndToEndDesc desc,
 /// @param options The compilation options.
 void registerEndToEndSuite(std::string suiteName,
                            std::vector<EndToEndDesc> descriptions,
-                           mlir::concretelang::CompilationOptions options) {
+                           mlir::concretelang::CompilationOptions options,
+                           int retryFailingTests) {
   for (auto desc : descriptions) {
-    registerEndToEnd(suiteName, desc, options);
+    registerEndToEnd(suiteName, desc, options, retryFailingTests);
   }
 }
 
@@ -200,10 +225,12 @@ int main(int argc, char **argv) {
 
   auto compilationOptions = std::get<0>(options);
   auto descriptionFiles = std::get<1>(options);
+  auto retryFailingTests = std::get<2>(options);
 
   for (auto descFile : descriptionFiles) {
     auto suiteName = path::stem(descFile.path).str() + ".library";
-    registerEndToEndSuite(suiteName, descFile.descriptions, compilationOptions);
+    registerEndToEndSuite(suiteName, descFile.descriptions, compilationOptions,
+                          retryFailingTests);
   }
   return RUN_ALL_TESTS();
 }
