f33631de4746f01fbd87d737ba389f44c889f438
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2023-12-18 10:09:20 +0100

    test(backend-cpu): Remove useless zip tests

--- backends/concrete-cpu/implementation/test/all.zig
+++
@@ -1,8 +0,0 @@
-comptime {
-    _ = @import("test_bit_extract_simulated.zig");
-    _ = @import("test_vertical_packing_simulated.zig");
-    _ = @import("test_encryption.zig");
-    _ = @import("test_bootstrap.zig");
-    _ = @import("test_bit_extract.zig");
-    _ = @import("test_vertical_packing.zig");
-}
