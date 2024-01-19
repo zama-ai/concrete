e78883cc2443847d2ab96f67fa652522251f326b
Andi Drebes <andi.drebes@zama.ai>
2024-01-19 15:43:58 +0100

    feat(compiler): Add a dialect for type inference debugging
    The `TypeInference` dialect provides three operations.
    
    The operation `TypeInference.propagate_downwards` respresents a type
    barrier, which is supposed to forward the type of its operand as its
    result type during type inference.
    
    The operation `TypeInference.propagate_upwards` also respresents a
    type barrier, but is supposed to forward the type of its result as the
    type for its operand during type inference.
    
    The operation `TypeInference.unresolved_conflict` can be used as a
    marker when two different types have beed inferred for a value (e.g.,
    one type during forward dataflow analysis and the other during
    backward dataflow analysis)

---
+++ compilers/concrete-compiler/compiler/include/concretelang/Dialect/TypeInference/IR/TypeInferenceDialect.h
@@ -0,0 +1,15 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_DIALECT_TYPEINFERENCE_IR_TYPEINFERENCEDIALECT_H
+#define CONCRETELANG_DIALECT_TYPEINFERENCE_IR_TYPEINFERENCEDIALECT_H
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+
+#include "concretelang/Dialect/TypeInference/IR/TypeInferenceOpsDialect.h.inc"
+
+#endif
---
+++ compilers/concrete-compiler/compiler/include/concretelang/Dialect/TypeInference/IR/TypeInferenceOps.h
@@ -0,0 +1,16 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifndef CONCRETELANG_DIALECT_TYPEINFERENCE_IR_TYPEINFERENCEOPS_H
+#define CONCRETELANG_DIALECT_TYPEINFERENCE_IR_TYPEINFERENCEOPS_H
+
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+
+#define GET_OP_CLASSES
+#include "concretelang/Dialect/TypeInference/IR/TypeInferenceOps.h.inc"
+
+#endif
