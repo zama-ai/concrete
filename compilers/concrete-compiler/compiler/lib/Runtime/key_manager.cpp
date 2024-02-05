0e7ccd09f611ef8710cd19f350585234eee2325d
Antoniu Pop <antoniu.pop@zama.ai>
2024-02-05 13:23:24 +0000

    feat(compiler): distributed execution - on-demand key transfer to remote nodes.

---
+++ compilers/concrete-compiler/compiler/lib/Runtime/key_manager.cpp
@@ -0,0 +1,39 @@
+// Part of the Concrete Compiler Project, under the BSD3 License with Zama
+// Exceptions. See
+// https://github.com/zama-ai/concrete-compiler-internal/blob/main/LICENSE.txt
+// for license information.
+
+#ifdef CONCRETELANG_DATAFLOW_EXECUTION_ENABLED
+
+#include "concretelang/Runtime/key_manager.hpp"
+#include "concretelang/Common/Keysets.h"
+#include "concretelang/Runtime/context.h"
+
+namespace mlir {
+namespace concretelang {
+namespace dfr {
+
+RuntimeContextManager *_dfr_node_level_runtime_context_manager;
+
+KeyWrapper<LweKeyswitchKey> getKsk(size_t keyId) {
+  return KeyWrapper<LweKeyswitchKey>(std::vector<LweKeyswitchKey>{
+      _dfr_node_level_runtime_context_manager->context->getKeys()
+          .lweKeyswitchKeys[keyId]});
+}
+
+KeyWrapper<LweBootstrapKey> getBsk(size_t keyId) {
+  return KeyWrapper<LweBootstrapKey>(std::vector<LweBootstrapKey>{
+      _dfr_node_level_runtime_context_manager->context->getKeys()
+          .lweBootstrapKeys[keyId]});
+}
+
+KeyWrapper<PackingKeyswitchKey> getPKsk(size_t keyId) {
+  return KeyWrapper<PackingKeyswitchKey>(std::vector<PackingKeyswitchKey>{
+      _dfr_node_level_runtime_context_manager->context->getKeys()
+          .packingKeyswitchKeys[keyId]});
+}
+
+} // namespace dfr
+} // namespace concretelang
+} // namespace mlir
+#endif
