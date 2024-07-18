4838d567b43c0bd47c410aa1e45b76e0f065dd48
youben11 <ayoub.benaissa@zama.ai>
2024-07-18 10:09:07 +0100

    feat(backend, compiler): convert TFHErs to Concrete ciphertext and back

---
+++ backends/concrete-cpu/implementation/src/c_api/fheint.rs
@@ -0,0 +1,179 @@
+use super::utils::nounwind;
+use core::slice;
+use std::io::Cursor;
+use tfhe::core_crypto::prelude::*;
+use tfhe::integer::ciphertext::Expandable;
+use tfhe::integer::IntegerCiphertext;
+use tfhe::shortint::parameters::{Degree, NoiseLevel};
+use tfhe::shortint::{CarryModulus, Ciphertext, MessageModulus};
+use tfhe::FheUint8;
+
+#[repr(C)]
+pub struct TfhersFheIntDescription {
+    width: usize,
+    is_signed: bool,
+    lwe_size: usize,
+    n_cts: usize,
+    degree: usize,
+    noise_level: usize,
+    message_modulus: usize,
+    carry_modulus: usize,
+    ks_first: bool,
+}
+
+impl TfhersFheIntDescription {
+    fn zero() -> TfhersFheIntDescription {
+        TfhersFheIntDescription {
+            width: 0,
+            is_signed: false,
+            lwe_size: 0,
+            n_cts: 0,
+            degree: 0,
+            noise_level: 0,
+            message_modulus: 0,
+            carry_modulus: 0,
+            ks_first: false,
+        }
+    }
+
+    /// Create a `Ciphertext` from the lwe and `self` metadata
+    fn ct_from_lwe(&self, lwe: LweCiphertext<Vec<u64>>) -> Ciphertext {
+        Ciphertext::new(
+            lwe,
+            Degree::new(self.degree),
+            // TODO: how to set noise here?
+            NoiseLevel::UNKNOWN,
+            MessageModulus(self.message_modulus),
+            CarryModulus(self.carry_modulus),
+            if self.ks_first {
+                PBSOrder::KeyswitchBootstrap
+            } else {
+                PBSOrder::BootstrapKeyswitch
+            },
+        )
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn concrete_cpu_tfhers_uint8_description(
+    serialized_data_ptr: *const u8,
+    serialized_data_len: usize,
+) -> TfhersFheIntDescription {
+    nounwind(|| {
+        // deserialize fheuint8
+        let mut serialized_data = Cursor::new(slice::from_raw_parts(
+            serialized_data_ptr,
+            serialized_data_len,
+        ));
+        let fheuint: FheUint8 = match bincode::deserialize_from(&mut serialized_data) {
+            Ok(value) => value,
+            Err(_) => {
+                return TfhersFheIntDescription::zero();
+            }
+        };
+        // get metadata from fheuint's ciphertext
+        let (radix, _) = fheuint.into_raw_parts();
+        let blocks = radix.blocks();
+        let ct = match blocks.first() {
+            Some(value) => &value.ct,
+            None => {
+                return TfhersFheIntDescription::zero();
+            }
+        };
+        TfhersFheIntDescription {
+            width: 8,
+            is_signed: false,
+            lwe_size: ct.lwe_size().0,
+            n_cts: blocks.len(),
+            degree: blocks[0].degree.get(),
+            noise_level: blocks[0].noise_level().get(),
+            message_modulus: blocks[0].message_modulus.0,
+            carry_modulus: blocks[0].carry_modulus.0,
+            ks_first: blocks[0].pbs_order == PBSOrder::KeyswitchBootstrap,
+        }
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn concrete_cpu_tfhers_uint8_to_lwe_array(
+    serialized_data_ptr: *const u8,
+    serialized_data_len: usize,
+    lwe_vec_buffer: *mut u64,
+) -> i64 {
+    nounwind(|| {
+        // deserialize fheuint8
+        let mut serialized_data = Cursor::new(slice::from_raw_parts(
+            serialized_data_ptr,
+            serialized_data_len,
+        ));
+        // TODO: can we have a generic deserialize?
+        let fheuint: FheUint8 = match bincode::deserialize_from(&mut serialized_data) {
+            Ok(value) => value,
+            Err(_) => {
+                return 1;
+            }
+        };
+        // collect LWEs from fheuint
+        let (radix, _) = fheuint.into_raw_parts();
+        let blocks = radix.blocks();
+        let first_ct = match blocks.first() {
+            Some(value) => &value.ct,
+            None => return 1,
+        };
+        let lwe_size = first_ct.lwe_size().0;
+        let n_cts = blocks.len();
+        // copy LWEs to C buffer. Note that lsb is cts[0]
+        let lwe_vector: &mut [u64] = slice::from_raw_parts_mut(lwe_vec_buffer, n_cts * lwe_size);
+        for (i, block) in blocks.iter().enumerate() {
+            lwe_vector[i * lwe_size..(i + 1) * lwe_size]
+                .copy_from_slice(block.ct.clone().into_container().as_slice());
+        }
+        0
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn concrete_cpu_lwe_array_to_tfhers_uint8(
+    lwe_vec_buffer: *const u64,
+    fheuint_buffer: *mut u8,
+    fheuint_buffer_size: usize,
+    fheuint_desc: TfhersFheIntDescription,
+) -> usize {
+    nounwind(|| {
+        let lwe_size = fheuint_desc.lwe_size;
+        let n_cts = fheuint_desc.n_cts;
+        // construct fheuint from LWEs
+        let lwe_vector: &[u64] = slice::from_raw_parts(lwe_vec_buffer, n_cts * lwe_size);
+        let mut blocks: Vec<Ciphertext> = Vec::new();
+        for i in 0..n_cts {
+            let lwe_ct = LweCiphertext::<Vec<u64>>::from_container(
+                lwe_vector[i * lwe_size..(i + 1) * lwe_size].to_vec(),
+                CiphertextModulus::new_native(),
+            );
+            blocks.push(fheuint_desc.ct_from_lwe(lwe_ct));
+        }
+        let fheuint = match FheUint8::from_expanded_blocks(
+            blocks,
+            tfhe::integer::ciphertext::DataKind::Unsigned(4),
+        ) {
+            Ok(value) => value,
+            Err(_) => {
+                return 0;
+            }
+        };
+        // serialize fheuint
+        let mut serialized_data = Vec::new();
+        match bincode::serialize_into(&mut serialized_data, &fheuint) {
+            Ok(_) => (),
+            Err(_) => {
+                return 0;
+            }
+        }
+        if serialized_data.len() > fheuint_buffer_size {
+            return 0;
+        }
+        let buff: &mut [u8] = slice::from_raw_parts_mut(fheuint_buffer, serialized_data.len());
+        buff.copy_from_slice(&serialized_data);
+        serialized_data.len()
+    })
+}
---
+++ compilers/concrete-compiler/compiler/lib/Bindings/Python/concrete/compiler/tfhers_int.py
@@ -0,0 +1,230 @@
+"""Import and export TFHErs integers into Concrete."""
+
+# pylint: disable=no-name-in-module,import-error
+
+from mlir._mlir_libs._concretelang._compiler import (
+    import_tfhers_fheuint8 as _import_tfhers_fheuint8,
+    export_tfhers_fheuint8 as _export_tfhers_fheuint8,
+    get_tfhers_fheuint8_description as _get_tfhers_fheuint8_description,
+    TfhersFheIntDescription as _TfhersFheIntDescription,
+)
+from .value import Value
+from .wrapper import WrapperCpp
+
+# pylint: enable=no-name-in-module,import-error
+
+
+class TfhersFheIntDescription(WrapperCpp):
+    """A helper class to create `TfhersFheIntDescription`s."""
+
+    def __init__(self, desc: _TfhersFheIntDescription):
+        """
+        Wrap the native C++ object.
+
+        Args:
+            desc (_TfhersFheIntDescription):
+                object to wrap
+
+        Raises:
+            TypeError:
+                if `desc` is not of type `_TfhersFheIntDescription`
+        """
+
+        if not isinstance(desc, _TfhersFheIntDescription):
+            raise TypeError(
+                f"desc must be of type _TfhersFheIntDescription, not {type(desc)}"
+            )
+
+        super().__init__(desc)
+
+    @staticmethod
+    # pylint: disable=arguments-differ
+    def new(
+        width: int,
+        is_signed: bool,
+        message_modulus: int,
+        carry_modulus: int,
+        degree: int,
+        lwe_size: int,
+        n_cts: int,
+        noise_level: int,
+        ks_first: bool,
+    ) -> "TfhersFheIntDescription":
+        """Create a TfhersFheIntDescription.
+
+        Args:
+            width (int): integer width
+            is_signed (bool): signed or unsigned
+            message_modulus (int): message modulus (not its log2)
+            carry_modulus (int): carry modulus (not its log2)
+            degree (int): degree
+            lwe_size (int): LWE size
+            n_cts (int): number of ciphertexts
+            noise_level (int): noise level
+            ks_first (bool): PBS order (keyswitch first, or bootstrap first)
+
+        Returns:
+            TfhersFheIntDescription: TFHErs integer description
+        """
+        return TfhersFheIntDescription(
+            _TfhersFheIntDescription(
+                width,
+                is_signed,
+                message_modulus,
+                carry_modulus,
+                degree,
+                lwe_size,
+                n_cts,
+                noise_level,
+                ks_first,
+            )
+        )
+
+    @property
+    def width(self) -> int:
+        """Total integer bitwidth"""
+        return self.cpp().width
+
+    @width.setter
+    def width(self, width: int):
+        self.cpp().width = width
+
+    @property
+    def is_signed(self) -> bool:
+        """Is the integer signed"""
+        return self.cpp().is_signed
+
+    @is_signed.setter
+    def is_signed(self, is_signed: bool):
+        self.cpp().is_signed = is_signed
+
+    @property
+    def message_modulus(self) -> int:
+        """Modulus of the message part in each ciphertext"""
+        return self.cpp().message_modulus
+
+    @message_modulus.setter
+    def message_modulus(self, message_modulus: int):
+        self.cpp().message_modulus = message_modulus
+
+    @property
+    def carry_modulus(self) -> int:
+        """Modulus of the carry part in each ciphertext"""
+        return self.cpp().carry_modulus
+
+    @carry_modulus.setter
+    def carry_modulus(self, carry_modulus: int):
+        self.cpp().carry_modulus = carry_modulus
+
+    @property
+    def degree(self) -> int:
+        """Tracks the number of operations that have been done"""
+        return self.cpp().degree
+
+    @degree.setter
+    def degree(self, degree: int):
+        self.cpp().degree = degree
+
+    @property
+    def lwe_size(self) -> int:
+        """LWE size"""
+        return self.cpp().lwe_size
+
+    @lwe_size.setter
+    def lwe_size(self, lwe_size: int):
+        self.cpp().lwe_size = lwe_size
+
+    @property
+    def n_cts(self) -> int:
+        """Number of ciphertexts"""
+        return self.cpp().n_cts
+
+    @n_cts.setter
+    def n_cts(self, n_cts: int):
+        self.cpp().n_cts = n_cts
+
+    @property
+    def noise_level(self) -> int:
+        """Noise level"""
+        return self.cpp().noise_level
+
+    @noise_level.setter
+    def noise_level(self, noise_level: int):
+        self.cpp().noise_level = noise_level
+
+    @property
+    def ks_first(self) -> bool:
+        """Keyswitch placement relative to the bootsrap in a PBS"""
+        return self.cpp().ks_first
+
+    @ks_first.setter
+    def ks_first(self, ks_first: bool):
+        self.cpp().ks_first = ks_first
+
+    def __str__(self) -> str:
+        return (
+            f"tfhers_int_description<width={self.width}, signed={self.is_signed}, msg_mod="
+            f"{self.message_modulus}, carry_mod={self.carry_modulus}, degree={self.degree}, "
+            f"lwe_size={self.lwe_size}, n_cts={self.n_cts}, noise_level={self.noise_level} "
+            f"ks_first={self.ks_first}>"
+        )
+
+    @staticmethod
+    def from_serialized_fheuint8(buffer: bytes) -> "TfhersFheIntDescription":
+        """Get the description of a serialized TFHErs fheuint8.
+
+        Args:
+            buffer (bytes): serialized fheuint8
+
+        Raises:
+            TypeError: buffer is not of type bytes
+
+        Returns:
+            TfhersFheIntDescription: description of the serialized fheuint8
+        """
+        if not isinstance(buffer, bytes):
+            raise TypeError(f"buffer must be of type bytes, not {type(buffer)}")
+        return TfhersFheIntDescription.wrap(_get_tfhers_fheuint8_description(buffer))
+
+
+class TfhersExporter:
+    """A helper class to import and export TFHErs big integers."""
+
+    @staticmethod
+    def export_fheuint8(value: Value, info: TfhersFheIntDescription) -> bytes:
+        """Convert Concrete value to TFHErs and serialize it.
+
+        Args:
+            value (Value): value to export
+            info (TfhersFheIntDescription): description of the TFHErs integer to export to
+
+        Raises:
+            TypeError: if wrong input types
+
+        Returns:
+            bytes: converted and serialized fheuint8
+        """
+        if not isinstance(value, Value):
+            raise TypeError(f"value must be of type Value, not {type(value)}")
+        if not isinstance(info, TfhersFheIntDescription):
+            raise TypeError(
+                f"info must be of type TfhersFheIntDescription, not {type(info)}"
+            )
+        return bytes(_export_tfhers_fheuint8(value.cpp(), info.cpp()))
+
+    @staticmethod
+    def import_fheuint8(buffer: bytes) -> Value:
+        """Unserialize and convert from TFHErs to Concrete value.
+
+        Args:
+            buffer (bytes): serialized fheuint8
+
+        Raises:
+            TypeError: if wrong input types
+
+        Returns:
+            Value: unserialized and converted value
+        """
+        if not isinstance(buffer, bytes):
+            raise TypeError(f"buffer must be of type bytes, not {type(buffer)}")
+        return Value.wrap(_import_tfhers_fheuint8(buffer))
