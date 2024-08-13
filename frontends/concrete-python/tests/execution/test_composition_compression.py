b2e44e89701e9fe8c47ed1967940619c685c5101
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2024-08-13 17:24:26 +0200

    test(compiler/clientlib): Add test of decryption of a freshly compressed ciphertext

--- frontends/concrete-python/tests/execution/test_composition_compression.py
+++ frontends/concrete-python/tests/execution/test_composition_compression.py
@@ -24,3 +24,26 @@ def test_composable_with_input_compression(helpers):
     circuit = f.compile(fhe.inputset(fhe.uint3), conf)
     result = circuit.run(circuit.run(circuit.encrypt(2)))
     assert circuit.decrypt(result) == f(f(2))
+
+
+def test_composable_with_input_compression_decrypt_fresh_encrypted(helpers):
+    """
+    Test that we can decrypt a value which has just been encrypted with compression,
+    it requires composability as we need to ensure input and output partitions are the same.
+
+    https://github.com/zama-ai/concrete-internal/issues/758
+    """
+    conf = helpers.configuration()
+    if conf.parameter_selection_strategy != fhe.ParameterSelectionStrategy.MULTI:
+        # Composability is for now only valid with multi
+        return
+
+    @fhe.compiler({"x": "encrypted"})
+    def f(x):
+        return x**2
+
+    inputset = [2, 3, 4, 5]
+    conf.composable = True
+    conf.compress_input_ciphertexts = True
+    circuit = f.compile(inputset, conf)
+    assert circuit.decrypt(circuit.encrypt(2)) == 2
