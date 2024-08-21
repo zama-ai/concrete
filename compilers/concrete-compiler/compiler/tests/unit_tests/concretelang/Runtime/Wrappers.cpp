0a97006f64bd5844bcbbcd2508f5c7019a4e351f
youben11 <ayoub.benaissa@zama.ai>
2024-08-21 11:56:47 +0100

    fix(compiler): use checked_malloc for better error reporting

---
+++ compilers/concrete-compiler/compiler/tests/unit_tests/concretelang/Runtime/Wrappers.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "concretelang/Runtime/wrappers.h"
+
+namespace {
+
+TEST(WrappersDeathTest, bad_alloc) {
+  ASSERT_DEATH({ concrete_checked_malloc(SIZE_MAX); },
+               "bad alloc: nullptr while calling "
+               "malloc\\(17179869183 GB\\).*Backtrace:.*");
+}
+
+} // namespace
