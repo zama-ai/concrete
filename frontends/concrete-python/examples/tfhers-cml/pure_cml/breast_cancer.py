1b524be4768161e3e61ef6f255d6f0c22f0b3200
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2024-10-08 18:42:59 +0200

    docs(frontend): linear model

--- frontends/concrete-python/examples/tfhers-cml/pure_cml/breast_cancer.py
+++ frontends/concrete-python/examples/tfhers-cml/pure_cml/breast_cancer.py
@@ -3,14 +3,14 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-from concrete.ml.sklearn import XGBClassifier, RandomForestClassifier, DecisionTreeClassifier
+from concrete.ml.sklearn import LogisticRegression
 
 
 # Train the model
 X, y = load_breast_cancer(return_X_y=True)
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=1)
 
-model = XGBClassifier(n_bits=2, n_estimators=8, max_depth=3)
+model = LogisticRegression(n_bits=8)
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
@@ -18,18 +18,19 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy (all-in-one API): {accuracy*100:.2f}%")
 
 # Compile the model
-model.compile(x_train, show_mlir=False)
+model.compile(x_train, show_mlir=True)
 
 # Run with individual APIs
 x_test_q = model.quantize_input(x_test.astype(np.float32))
 y_pred_q = model._inference(x_test_q)
-y_pred_q_dequantized = model.dequantize_output(y_pred_q)
+y_pred_q_dequantized = model.dequantize_output(y_pred_q) > 0
 
-print(f"{y_pred_q_dequantized.shape=}")
-print(f"{y_test.shape=}")
+# print(f"{x_test_q.shape}")
+# print(f"{y_pred_q_dequantized.shape=}")
+# print(f"{y_test.shape=}")
 
-print(f"{y_pred_q_dequantized=}")
-print(f"{y_test=}")
+# print(f"{y_pred_q_dequantized=}")
+# print(f"{y_test=}")
 
 accuracy = accuracy_score(y_test, y_pred_q_dequantized)
 print(f"Accuracy (individual APIs): {accuracy*100:.2f}%")
