c871364acb88c0058cbe3736ac33e6f402d0353b
Alexandre Péré <alexandre.pere@zama.ai>
2024-10-21 09:52:05 +0200

    fix(optimizer): please new rust lints

--- compilers/concrete-optimizer/brute-force-optimizer/src/ks_free.rs
+++ compilers/concrete-optimizer/brute-force-optimizer/src/ks_free.rs
@@ -185,21 +185,18 @@ pub fn write_to_file(
         intem,
     } in res.iter()
     {
-        match intem {
-            Some((solution, cost)) => {
-                writeln!(
-                    writer,
-                    " {:2},     {:2}, {:2},  {:4},   {:2},  {:2},   {:6}",
-                    precision,
-                    log_norm,
-                    solution.glwe_dim,
-                    solution.log_poly_size,
-                    solution.level_pbs,
-                    solution.base_log_pbs,
-                    cost
-                )?;
-            }
-            None => {}
+        if let Some((solution, cost)) = intem {
+            writeln!(
+                writer,
+                " {:2},     {:2}, {:2},  {:4},   {:2},  {:2},   {:6}",
+                precision,
+                log_norm,
+                solution.glwe_dim,
+                solution.log_poly_size,
+                solution.level_pbs,
+                solution.base_log_pbs,
+                cost
+            )?;
         }
     }
     Ok(())
