92499c087dd1a3150785ab97ddfc6849b5c0ff4a
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2024-11-22 09:29:38 +0100

    fix(frontend-python): Ignore pkg_resources warning

--- frontends/concrete-python/concrete/__init__.py
+++ frontends/concrete-python/concrete/__init__.py
@@ -2,6 +2,10 @@
 Setup concrete namespace.
 """
 
+import warnings
+
 # Do not modify, this is to have a compatible namespace package
 # https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#pkg-resources-style-namespace-packages
-__import__("pkg_resources").declare_namespace(__name__)  # pragma: no cover
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    __import__("pkg_resources").declare_namespace(__name__)  # pragma: no cover
