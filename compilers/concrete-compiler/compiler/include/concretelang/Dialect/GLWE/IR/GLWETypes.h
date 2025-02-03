7d6302e2a58043838016a688fc9b884b21a5ca16
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2025-02-03 16:08:49 +0100

    feat(compiler/optimizer): Introduce glwe.glev type in GLWE Dialect

--- compilers/concrete-compiler/compiler/include/concretelang/Dialect/GLWE/IR/GLWETypes.h
+++ compilers/concrete-compiler/compiler/include/concretelang/Dialect/GLWE/IR/GLWETypes.h
@@ -15,6 +15,44 @@
 
 #include "concretelang/Dialect/GLWE/IR/GLWEAttrs.h"
 
+namespace mlir {
+
+template <>
+struct FieldParser<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>> {
+  static FailureOr<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>>
+  parse(AsmParser &parser) {
+    if (parser.parseLParen())
+      return failure();
+    ::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr> elements;
+    auto elementParser = [&]() {
+      auto element =
+          FieldParser<concretelang::GLWE::GLWEExprAttr>::parse(parser);
+      if (failed(element))
+        return failure();
+      elements.push_back(*element);
+      return success();
+    };
+    if (parser.parseCommaSeparatedList(elementParser))
+      return failure();
+    if (parser.parseRParen())
+      return failure();
+
+    return elements;
+  }
+};
+
+template <>
+void AsmPrinter::printStrippedAttrOrType<concretelang::GLWE::GLWEExprAttr>(
+    ::llvm::ArrayRef<concretelang::GLWE::GLWEExprAttr> attrOrTypes) {
+  getStream() << "(";
+  llvm::interleaveComma(attrOrTypes, getStream(),
+                        [this](concretelang::GLWE::GLWEExprAttr attrOrType) {
+                          printStrippedAttrOrType(attrOrType);
+                        });
+  getStream() << ")";
+}
+} // namespace mlir
+
 #define GET_TYPEDEF_CLASSES
 #include "concretelang/Dialect/GLWE/IR/GLWETypes.h.inc"
 
