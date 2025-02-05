4b32d58ad1daad5f7d8217772484f58f5e163b60
Quentin Bourgerie <bourgerie.quentin@gmail.com>
2025-02-05 15:37:49 +0100

    fix(compiler/glwe): Move printer template specializations to avoid multiple definition

--- compilers/concrete-compiler/compiler/include/concretelang/Dialect/GLWE/IR/GLWETypes.h
+++ compilers/concrete-compiler/compiler/include/concretelang/Dialect/GLWE/IR/GLWETypes.h
@@ -9,49 +9,10 @@
 #include "llvm/ADT/TypeSwitch.h"
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/BuiltinTypes.h>
-#include <mlir/IR/DialectImplementation.h>
 
 #include <mlir/Dialect/Arith/IR/Arith.h>
 
 #include "concretelang/Dialect/GLWE/IR/GLWEAttrs.h"
-
-namespace mlir {
-
-template <>
-struct FieldParser<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>> {
-  static FailureOr<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>>
-  parse(AsmParser &parser) {
-    if (parser.parseLParen())
-      return failure();
-    ::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr> elements;
-    auto elementParser = [&]() {
-      auto element =
-          FieldParser<concretelang::GLWE::GLWEExprAttr>::parse(parser);
-      if (failed(element))
-        return failure();
-      elements.push_back(*element);
-      return success();
-    };
-    if (parser.parseCommaSeparatedList(elementParser))
-      return failure();
-    if (parser.parseRParen())
-      return failure();
-
-    return elements;
-  }
-};
-
-template <>
-void AsmPrinter::printStrippedAttrOrType(
-    ::llvm::ArrayRef<concretelang::GLWE::GLWEExprAttr> attrOrTypes) {
-  getStream() << "(";
-  llvm::interleaveComma(attrOrTypes, getStream(),
-                        [this](concretelang::GLWE::GLWEExprAttr attrOrType) {
-                          printStrippedAttrOrType(attrOrType);
-                        });
-  getStream() << ")";
-}
-} // namespace mlir
 
 #define GET_TYPEDEF_CLASSES
 #include "concretelang/Dialect/GLWE/IR/GLWETypes.h.inc"
--- compilers/concrete-compiler/compiler/lib/Dialect/GLWE/IR/GLWEDialect.cpp
+++ compilers/concrete-compiler/compiler/lib/Dialect/GLWE/IR/GLWEDialect.cpp
@@ -11,6 +11,44 @@
 
 // #include "concretelang/Dialect/GLWE/Interfaces/GLWEInterfaces.h"
 
+namespace mlir {
+
+template <>
+struct FieldParser<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>> {
+  static FailureOr<::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr>>
+  parse(AsmParser &parser) {
+    if (parser.parseLParen())
+      return failure();
+    ::llvm::SmallVector<concretelang::GLWE::GLWEExprAttr> elements;
+    auto elementParser = [&]() {
+      auto element =
+          FieldParser<concretelang::GLWE::GLWEExprAttr>::parse(parser);
+      if (failed(element))
+        return failure();
+      elements.push_back(*element);
+      return success();
+    };
+    if (parser.parseCommaSeparatedList(elementParser))
+      return failure();
+    if (parser.parseRParen())
+      return failure();
+
+    return elements;
+  }
+};
+
+template <>
+void AsmPrinter::printStrippedAttrOrType(
+    ::llvm::ArrayRef<concretelang::GLWE::GLWEExprAttr> attrOrTypes) {
+  getStream() << "(";
+  llvm::interleaveComma(attrOrTypes, getStream(),
+                        [this](concretelang::GLWE::GLWEExprAttr attrOrType) {
+                          printStrippedAttrOrType(attrOrType);
+                        });
+  getStream() << ")";
+}
+} // namespace mlir
+
 #define GET_ATTRDEF_CLASSES
 #include "concretelang/Dialect/GLWE/IR/GLWEAttrs.cpp.inc"
 
